131e5b8ac49f7e7b69b0e71ad700f804f00fda71
sergiblanch <sergiblanch@users.sourceforge.net>
2010-03-29 12:59:35 +0000

    added 2 extra attributes (Calibration,Labels) without set anything the behavior doesn't change. Calibration means a set of fussy areas in the range of the allowed movent. It's an string (until we support double arrays) of triples, each has the [min, cal, max], write is to the calibrated position and reads return the position if it's in the interval. Labels is an string (list, when support) about the user talks, it contains the meaning of each possible position.
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@1360 381f683f-204f-49a0-b5f1-b96b46cb89f0

--- Pool/ctrls/lib/python/ioregister/TangoAttrIORCtrl.py
+++ Pool/ctrls/lib/python/ioregister/TangoAttrIORCtrl.py
@@ -1,34 +1,114 @@
+#############################################################################
+##
+## file :    TangoAttrIORCtrl.py
+##
+## description : 
+##
+## project :    Sardana/Pool/ctrls/OIRegister
+##
+## developers history: gcuni,sblanch
+##
+## copyleft :    Cells / Alba Synchrotron
+##               Bellaterra
+##               Spain
+##
+#############################################################################
+##
+## This file is part of Sardana.
+##
+## This is free software; you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+##
+## This software is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; if not, see <http://www.gnu.org/licenses/>.
+###########################################################################
+
 from PyTango import DevState
 from pool import IORegisterController
 from pool import PoolUtil
 
 TANGO_ATTR = 'TangoAttribute'
 DEVICE = 'Device'
+DPROXY = 'DeviceProxy'
 ATTRIBUTE = 'Attribute'
+CALIBRATION = 'Calibration'
+LABELS = 'Labels'
+READFAILED = 'readFailed'
+
+def agrupate(l,n):
+    #print "agrupate(%s,%s) "%(l,n)
+    res = []
+    if not len(l)%n == 0: raise Exception("Impossible for this length.")
+    for i in range(len(l)/n):
+        sub = []
+        for j in range(n):
+            sub.append(l[(i*n)+j])
+        res.append(sub)
+    return res
+
+def flatten(l):
+    #print "flatten(%s) "%(l)
+    result = []
+    for el in l:
+        if hasattr(el, "__iter__") and not isinstance(el, basestring):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
 
 
 class TangoAttrIORController(IORegisterController):
     """This controller offers as many channels as the user wants.
-    Each channel has two _MUST_HAVE_ extra attributes:
+    Each channel _MUST_HAVE_ extra attributes:
     +) TangoAttribute - Tango attribute to retrieve the value of the counter
     As examples you could have:
     ch1.TangoExtraAttribute = 'my/tango/device/attribute1'
     ch2.TangoExtraAttribute = 'my/tango/device/attribute2'
     ch3.TangoExtraAttribute = 'my_other/tango/device/attribute1'
+    Each channel _MAY_HAVE_ extra attributes:
+    +) Calibration - String of the triples min,cal,max
+    As examples you could have:
+    ch1.Calibration = "-50 0 50 950 1000 1050 1950 2000 2050 2950 3000 3050 3950 4000 4050 4950 5000 5050"
+    That means: [[-50.0, 0.0, 50.0],
+                 [950.0, 1000.0, 1050.0],
+                 [1950.0, 2000.0, 2050.0],
+                 [2950.0, 3000.0, 3050.0],
+                 [3950.0, 4000.0, 4050.0],
+                 [4950.0, 5000.0, 5050.0]]
+    +) Labels - Human readable way to understand the positions. This is what
+                the user will know about positions.
+    As examples you have:
+    ch1.Labels: "16.66% 33.33% 50% 66.66% 83.33% 100%"
+    That means: {0:'16.66%',1:'33.33%',2:'50%',3:'66.66%',4:'83.33%',5'100%']
     """
-                 
+
     gender = ""
     model  = ""
     organization = "CELLS - ALBA"
     image = ""
     icon = ""
     logo = "ALBA_logo.png"
-                     
+
     ctrl_extra_attributes ={TANGO_ATTR:
-                            {'Type':'PyTango.DevString'
-                             ,'Description':'The first Tango Attribute to read (e.g. my/tango/dev/attr)'
-                             ,'R/W Type':'PyTango.READ_WRITE'}
-                            }
+                            {'Type':'PyTango.DevString',
+                             'Description':'The first Tango Attribute to read (e.g. my/tango/dev/attr)',
+                             'R/W Type':'PyTango.READ_WRITE'},
+                            CALIBRATION:#type hackish until arrays supported
+                            {'Type':'PyTango.DevString',#{'Type':'PyTango.DevVarDoubleArray',
+                             'Description':'Flatten list of a list of triples and [min,cal,max]',
+                             'R/W Type':'PyTango.READ_WRITE'},
+                            LABELS:#type hackish until arrays supported
+                            {'Type':'PyTango.DevString',#{'Type':'PyTango.DevVarStringArray',
+                             'Description':'String list with the meaning of each discrete position',
+                             'R/W Type':'PyTango.READ_WRITE'},
+                           }
     MaxDevice = 1024
 
     def __init__(self, inst, props):
@@ -39,44 +119,137 @@ class TangoAttrIORController(IORegisterController):
         self._log.debug('AddDevice %d' % axis)
         self.devsExtraAttributes[axis] = {}
         self.devsExtraAttributes[axis][TANGO_ATTR] = None
+        self.devsExtraAttributes[axis][CALIBRATION] = []
+        self.devsExtraAttributes[axis][LABELS] = []
+        #When the ReadOne raise an exception, inform the state
+        self.devsExtraAttributes[axis][READFAILED] = False
 
     def DeleteDevice(self, axis):
         del self.devsExtraAttributes[axis]
 
     def StateOne(self, axis):
-        return (DevState.ON, 'Always ON, just reading external Tango Attribute')
+        tango_attr = self.devsExtraAttributes[axis][TANGO_ATTR]
+        llabels = len(self.devsExtraAttributes[axis][LABELS])
+        lcalibration = len(self.devsExtraAttributes[axis][CALIBRATION])
+        readFailed = self.devsExtraAttributes[axis][READFAILED]
+        if tango_attr == None:
+            return (DevState.DISABLE,
+                    "Not yet configured the Tango Attribute")
+        if not llabels == lcalibration:
+            return(DevState.DISABLE,
+                   "Bad configuration of the extra attributes, this cannot be operated")
+        else:
+            dev_proxy = self.devsExtraAttributes[axis][DPROXY]
+            dev_state = dev_proxy.state()
+            if readFailed and not dev_state == DevState.MOVING:
+                return (DevState.ALARM,
+                        "Fault on read attibute.\nThe tango device status says: %s"%dev_proxy.status())
+            return (dev_proxy.state(),
+                    "The tango device status says: %s"%dev_proxy.status())
+
+        
 
     def ReadOne(self, axis):
         dev = self.devsExtraAttributes[axis][DEVICE]
         attr = self.devsExtraAttributes[axis][ATTRIBUTE]
-        dev_proxy = PoolUtil().get_device(self.inst_name, dev)
+        dev_proxy = self.devsExtraAttributes[axis][DPROXY]
+        labels = self.devsExtraAttributes[axis][LABELS]
+        llabels = len(labels)
+        calibration = self.devsExtraAttributes[axis][CALIBRATION]
+        lcalibration = len(calibration)
         try:
-            value = dev_proxy.read_attribute(attr).value
-            return int(value)
+            if llabels == 0 and lcalibration == 0:
+                value = self.devsExtraAttributes[axis][DPROXY].read_attribute(attr).value
+                self.devsExtraAttributes[axis][READFAILED] = False
+                return int(value)
+            elif llabels == lcalibration:
+                value = dev_proxy.read_attribute(attr).value
+                for fussyPos in calibration:
+                    if value >= fussyPos[0] and value <= fussyPos[2]:
+                        self.devsExtraAttributes[axis][READFAILED] = False
+                        return int(calibration.index(fussyPos))
+                self.devsExtraAttributes[axis][READFAILED] = True
+                raise Exception("Invalid position.")
+            else:
+                raise Exception("Bad configuration on optional extra attributes.")
         except Exception,e:
             self._log.error('Exception reading attribute:%s.%s' % (dev,attr))
+            try: self.devsExtraAttributes[axis][READFAILED] = True
+            except: pass
 
     def WriteOne(self, axis, value):
         dev = self.devsExtraAttributes[axis][DEVICE]
         attr = self.devsExtraAttributes[axis][ATTRIBUTE]
-        dev_proxy = PoolUtil().get_device(self.inst_name, dev)
+        dev_proxy = self.devsExtraAttributes[axis][DPROXY]
+        labels = self.devsExtraAttributes[axis][LABELS]
+        llabels = len(labels)
+        calibration = self.devsExtraAttributes[axis][CALIBRATION]
+        lcalibration = len(calibration)
         try:
-            dev_proxy.write_attribute(attr, value)
+            if llabels == 0 and lcalibration == 0:
+                dev_proxy.write_attribute(attr, value)
+            elif llabels == lcalibration:
+                if value >= len(labels): raise Exception("Out of range.")
+                dev_proxy.write_attribute(attr,calibration[value][1])
         except Exception, e:
             self._log.error('Exception writing attribute:%s.%s' % (dev,attr))
 
     def GetExtraAttributePar(self, axis, name):
-        return self.devsExtraAttributes[axis][name]
+        #case to apply the correspondant method per each extra attr
+        return {TANGO_ATTR:self.getTangoAttr,
+                CALIBRATION:self.getCalibration,
+                LABELS:self.getLabels,
+               }[name](axis,name)
+        
 
     def SetExtraAttributePar(self,axis, name, value):
         self._log.debug('SetExtraAttributePar [%d] %s = %s' % (axis, name, value))
-        self.devsExtraAttributes[axis][name] = value
-        if name == TANGO_ATTR:
-            idx = value.rfind("/")
-            dev = value[:idx]
-            attr = value[idx+1:]
-            self.devsExtraAttributes[axis][DEVICE] = dev
-            self.devsExtraAttributes[axis][ATTRIBUTE] = attr
-        
+        #case to apply the correspondant method per each extra attr
+        {TANGO_ATTR:self.setTangoAttr,
+         CALIBRATION:self.setCalibration,
+         LABELS:self.setLabels,
+        }[name](axis,name,value)
+
     def SendToCtrl(self,in_data):
         return ""
+
+    ####
+    # Auxiliar methods for the extra attributes
+
+    def getTangoAttr(self,axis,name):
+        return self.devsExtraAttributes[axis][name]
+
+    def setTangoAttr(self,axis,name,value):
+        self.devsExtraAttributes[axis][name] = value
+        idx = value.rfind("/")
+        dev = value[:idx]
+        attr = value[idx+1:]
+        self.devsExtraAttributes[axis][DEVICE] = dev
+        self.devsExtraAttributes[axis][DPROXY] = PoolUtil().get_device(self.inst_name, dev)
+        self.devsExtraAttributes[axis][ATTRIBUTE] = attr
+
+    def getCalibration(self,axis,name):
+        bar = "".join("%s "%e for e in flatten(self.devsExtraAttributes[axis][CALIBRATION]))
+        return bar[:-1]
+        #hackish until we support DevVarDoubleArray in extra attrs
+        #return flatten(self.devsExtraAttributes[axis][CALIBRATION])
+
+    def setCalibration(self,axis,name,value):
+        #hackish until we support DevVarDoubleArray in extra attrs
+        value = value.split()
+        for i in range(len(value)): value[i] = float(value[i])
+        self.devsExtraAttributes[axis][CALIBRATION] = agrupate(value,3)
+
+    def getLabels(self,axis,name):
+        bar = "".join("%s "%e for e in self.devsExtraAttributes[axis][LABELS])
+        return bar[:-1]
+        #hackish until we support DevVarDoubleArray in extra attrs
+        #return self.devsExtraAttributes[axis][LABELS]
+
+    def setLabels(self,axis,name,value):
+        #hackish until we support DevVarStringArray in extra attrs
+        value = value.split()
+        self.devsExtraAttributes[axis][LABELS] = value
+
+    # end aux for extras
+    ####
