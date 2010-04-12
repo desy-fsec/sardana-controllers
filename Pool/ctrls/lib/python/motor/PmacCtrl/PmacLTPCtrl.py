ca7af5a9ec8e39a1a2beec0d1d089ba420b7cfb9
zreszela <zreszela@users.sourceforge.net>
2010-04-12 16:10:16 +0000

    added extra attr FeedbackMode
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@1408 381f683f-204f-49a0-b5f1-b96b46cb89f0

--- Pool/ctrls/lib/python/motor/PmacCtrl/PmacLTPCtrl.py
+++ Pool/ctrls/lib/python/motor/PmacCtrl/PmacLTPCtrl.py
@@ -38,6 +38,9 @@ from pool import MotorController
 
 class PmacLTPController(PmacController):
     """This class is the Tango Sardana motor controller for the Pmac motor controller device in LTP."""
+    
+    ctrl_extra_attributes = dict(PmacController.ctrl_extra_attributes)
+    ctrl_extra_attributes['FeedbackMode'] = {'Type':'PyTango.DevLong','R/W Type':'PyTango.READ_WRITE'}
 
     def __init__(self,inst,props):
         PmacController.__init__(self,inst,props)
@@ -61,3 +64,64 @@ class PmacLTPController(PmacController):
                state = PyTango.DevState.ALARM
                status = '''\nMotor's amplifier is not enabled'''
         return (state, status, switchstate)
+    
+    def GetExtraAttributePar(self, axis, name):
+        """ Get Pmac axis particular parameters.
+        @param axis to get the parameter
+        @param name of the parameter to retrive
+        @return the value of the parameter
+        """
+        name = name.lower()
+        if name == "feedbackmode":
+            if axis == 1:
+                i103value = self.pmacEth.command_inout("GetIVariable", int("%d03" % axis))
+                try:
+                    if int(i103value) == 13569:
+                        return 1
+                    elif int(i103value) == 13571:
+                        return 2
+                    else:
+                        self._log.error("While getting feedback mode Pmac returned some inconsistent value, please report it to controls division.")
+			PyTango.Except.throw_exception("Value error",
+                                           "Pmac returned some inconsistent value, please report it to controls division.",
+                                           "PmacLTPCtrl.GetExtraAttribute()")
+                except ValueError:
+                    self._log.error("While gettinh feedback mode Pmac returned some inconsistent value, please report it to controls division.")
+                    PyTango.Except.throw_exception("Value error",
+                                           "Pmac returned some inconsistent value, please report it to controls division.",
+                                           "PmacLTPCtrl.GetExtraAttribute()")
+            if axis == 2:
+                self._log.warning("Various feedback mode feature is reserved only for top axis.")
+		PyTango.Except.throw_exception("Value error",
+                                           "Axis nr 2 does not support various feedback mode.",
+                                           "PmacLTPCtrl.GetExtraAttribute()")
+        else:
+	    return PmacController.GetExtraAttributePar(self, axis, name)
+
+    def SetExtraAttributePar(self, axis, name, value):
+        """ Set Pmac axis particular parameters.
+        @param axis to set the parameter
+        @param name of the parameter
+        @param value to be set
+        """
+        name = name.lower()
+        if name == "feedbackmode":
+            if axis == 1:
+                if value == 1:
+                    self.pmacEth.command_inout("SetIVariable",[int("%d03" % axis), 13569])
+                elif value == 2:
+                    self.pmacEth.command_inout("SetIVariable",[int("%d03" % axis), 13571])
+                else:
+                    self._log.warning("Feedback supports only two modes: use 1 for single feedback mode or 2 for dual feedback mode.")
+                    PyTango.Except.throw_exception("Value error",
+                                           "Wrong value, use 1 for single feedback mode or 2 for dual feedback mode.",
+                                           "PmacLTPCtrl.SetExtraAttribute()")
+            else:
+                self._log.warning("Various feedback mode feature is reserved only for top axis.")
+                PyTango.Except.throw_exception("Value error",
+                                           "Axis nr 2 does not support various feedback mode.",
+                                           "PmacLTPCtrl.SetExtraAttribute()")
+
+                return
+        else:
+            PmacController.SetExtraAttributePar(self, axis, name, value)
