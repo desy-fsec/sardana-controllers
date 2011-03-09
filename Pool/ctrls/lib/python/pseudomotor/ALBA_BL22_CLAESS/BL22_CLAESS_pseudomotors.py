f319fcc10f5e12e7a96f381038e2148900b19455
zreszela <zreszela@users.sourceforge.net>
2011-03-09 10:29:18 +0000

    TripodTableController first commit
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@3206 381f683f-204f-49a0-b5f1-b96b46cb89f0

---
+++ Pool/ctrls/lib/python/pseudomotor/ALBA_BL22_CLAESS/BL22_CLAESS_pseudomotors.py
@@ -0,0 +1,143 @@
+import math
+from pool import PseudoMotorController
+from taurus.core.util import Singleton
+import logging
+
+
+def rotate_x(y, z, cosangle, sinangle):
+    """3D rotaion around *x* (pitch). *y* and *z* are values or arrays.
+    Positive rotation is for positive *sinangle*. Returns *yNew, zNew*."""
+    return cosangle * y - sinangle * z, sinangle * y + cosangle * z
+def rotate_y(x, z, cosangle, sinangle):
+    """3D rotaion around *y* (roll). *x* and *z* are values or arrays.
+    Positive rotation is for positive *sinangle*. Returns *xNew, zNew*."""
+    return cosangle * x + sinangle * z, -sinangle * x + cosangle * z
+def rotate_z(x, y, cosangle, sinangle):
+    """3D rotaion around *z*. *x* and *y* are values or arrays.
+    Positive rotation is for positive *sinangle*. Returns *xNew, yNew*."""
+    return cosangle * x - sinangle * y, sinangle * x + cosangle * y
+
+
+class TripodTableController(PseudoMotorController):
+
+    pseudo_motor_roles = ('z', 'pitch', 'roll')
+    motor_roles = ('jack1', 'jack2', 'jack3')
+
+    class_prop = { 'Jack1Coordinates' : {'Type' : 'PyTango.DevString', 'Description' : 'jack1 coordination: x,y,z'},
+                         'Jack2Coordinates' : {'Type' : 'PyTango.DevString', 'Description' : 'jack2 coordination: x,y,z'},
+                         'Jack3Coordinates' : {'Type' : 'PyTango.DevString', 'Description' : 'jack3 coordination: x,y,z'},
+                         'CenterCoordinates': {'Type' : 'PyTango.DevString', 'Description' : 'center coordination: x,y,z'}}
+
+    cosAzimuth = 0.70710681665463704
+    sinAzimuth = -0.70710674571845633
+
+    def __init__(self, inst, props):  
+        PseudoMotorController.__init__(self, inst, props)
+        self._log.setLevel(logging.DEBUG)
+
+        try:
+            self.jack1 = [float(c) for c in props['Jack1Coordinates'].split(',')]
+            self.jack2 = [float(c) for c in props['Jack2Coordinates'].split(',')]
+            self.jack3 = [float(c) for c in props['Jack3Coordinates'].split(',')]
+            self.center = [float(c) for c in props['CenterCoordinates'].split(',')]
+        except ValueError, e:
+            self._log.error('Could not parse class properties to generate coordinates.')
+            raise e  
+
+        if not (self. jack1[2] == self.jack2[2] == self.jack3[2]):
+            raise ValueError('The mirror must be initially horizontal!')
+
+        self.jackToMirrorInvariant = self.center[2] - self.jack1[2]
+
+        # jacks in local virgin system
+        self.jack1local = [ji - ci for ji, ci in zip(self.jack1, self.center)]
+        self.jack2local = [ji - ci for ji, ci in zip(self.jack2, self.center)]
+        self.jack3local = [ji - ci for ji, ci in zip(self.jack3, self.center)]
+
+        for jl in [self.jack1local, self.jack2local, self.jack3local]:
+            jl[0], jl[1] = rotate_z(jl[0], jl[1], self.cosAzimuth, self.sinAzimuth)
+        self._log.debug("jack1local: %s" %repr(self.jack1local))
+        self._log.debug("jack2local: %s" %repr(self.jack2local))
+        self._log.debug("jack3local: %s" %repr(self.jack3local))
+        
+    def calc_physical(self, index, pseudos):
+        return self.calc_all_physical(pseudos)[index - 1]
+    
+    def calc_pseudo(self, index, physicals):
+        return self.calc_all_pseudo(physicals)[index - 1]
+    
+    def calc_all_physical(self, pseudos):
+        self._log.debug("Entering calc_all_physical...")
+        z, pitch, roll = pseudos
+#      Ax + By + Cz = D in local system:
+        A, B, C = 0.0, 0.0, 1.0
+
+        if roll != 0:
+            cosRoll = math.cos(roll)
+            sinRoll = math.sin(roll)
+            A, C = rotate_y(A, C, cosRoll, sinRoll)
+        if pitch != 0:
+            cosPitch = math.cos(pitch)
+            sinPitch = math.sin(pitch)
+            B, C = rotate_x(B, C, cosPitch, sinPitch)
+
+#        D of optical plane = 0 because (0, 0, 0) belongs to it:
+        D = 0
+#      D of balls plane:
+#           D -= self.jackToMirrorInvariant * (A ** 2 + B ** 2 + C ** 2) ** 0.5
+#           but because rotations are unitary (A ** 2 + B ** 2 + C ** 2) = 1 and:
+        #D -= self.jackToMirrorInvariant
+        D -= (self.center[2] - z)
+
+        self._log.debug("Plane equation in local system A: %f, B: %f, C: %f, D: %f" % (A,B,C,D))
+
+        jack1_local = (D - A * self.jack1local[0] - B * self.jack1local[1]) / C 
+        jack2_local = (D - A * self.jack2local[0] - B * self.jack2local[1]) / C
+        jack3_local = (D - A * self.jack3local[0] - B * self.jack3local[1]) / C
+
+        self._log.debug("jack1_local: %s" %repr(jack1_local))
+        self._log.debug("jack2_local: %s" %repr(jack2_local))
+        self._log.debug("jack3_local: %s" %repr(jack3_local))
+
+        jack1 = jack1_local + self.center[2]
+        jack2 = jack2_local + self.center[2]
+        jack3 = jack3_local + self.center[2]
+
+        return jack1, jack2, jack3 
+
+    def calc_all_pseudo(self, physicals):
+        self._log.debug("Entering calc_all_pseudo...")
+        jack1, jack2, jack3 = physicals
+
+#      Ax + By + Cz = D in global system:
+        A = (self.jack2[1] - self.jack1[1]) * (jack3 - jack1) - (self.jack3[1] - self.jack1[1]) * (jack2 - jack1)
+        B = (self.jack3[0] - self.jack1[0]) * (jack2 - jack1) - (self.jack2[0] - self.jack1[0]) * (jack3 - jack1)
+        C = (self.jack2[0] - self.jack1[0]) * (self.jack3[1] - self.jack1[1])  - (self.jack3[0] - self.jack1[0]) * (self.jack2[1] - self.jack1[1])
+
+        self._log.debug(" A: %f, B: %f, C: %f" % (A,B,C))
+        ABCNorm = (A ** 2 + B ** 2 + C ** 2) ** 0.5
+        self._log.debug("ABCNorm: %f" % (ABCNorm))
+        if C < 0:
+            ABCNorm *= -1      # its normal looks upwards!
+        A /= ABCNorm; B /= ABCNorm; C /= ABCNorm
+        # D of balls plane
+        D = A * self.jack1[0] + B * self.jack1[1] + C * jack1
+
+        #jackToMirrorInvariant = self.center[2] - jack1
+        #D += self.jackToMirrorInvariant# of optical plane
+
+        self._log.debug("Plane equation in global system A: %f, B: %f, C: %f, D: %f" % (A,B,C,D))
+        # C is never 0, i.e. the normal to the optical element is never horizontal
+        z = (D - A * self.center[0] - B * self.center[1]) / C
+        self._log.debug("z: %f" % z)
+
+        # A  and B in local system (C is unchanged):
+        locA, locB = rotate_z(A, B, self.cosAzimuth, self.sinAzimuth)
+        self._log.debug("A, B in local subsystem (rotated on z axiz) A: %f, locB: %f" % (locA, locB))
+        tanRoll = locA / C
+        roll = math.atan(tanRoll)
+
+        tanPitch = -locB / (locA * math.sin(roll) + C * math.cos(roll))
+        pitch = math.atan(tanPitch)
+
+        return z, pitch, roll
