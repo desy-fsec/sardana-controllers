d261c69eb3c258bc6958dd55f501234b8d259222
zreszela <zreszela@users.sourceforge.net>
2011-03-30 09:20:21 +0000

    diag_slit_hor macro added - homing horizontal slits procedure
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@3295 381f683f-204f-49a0-b5f1-b96b46cb89f0

--- MacroServer/macros/ALBA_BL22_CLAESS/DIAG.py
+++ MacroServer/macros/ALBA_BL22_CLAESS/DIAG.py
@@ -82,6 +82,86 @@ class diag_homing_vert(Macro):
     def on_abort(self):
         pass
 
+class diag_homing_hor(Macro):
+    """ 
+    This macro does the homing of horizontal slits of the BL22-CLAESS Diagnostic Module.
+    Homing procedure is done in 3 steps:
+    1. Right blade is moved to its positive limit(software or hardware)
+    2. Left blade is moved to its negative limit(software or hardware)
+    3. Icepap homing procedure is started looking for home in the follwing maner:
+       - right blade moving negative
+       - left blade moving positive
+    In case of successfully homing of both blades macro returns True, on all other cases it return False
+    """
+
+    param_def = [
+        ['limits', Type.String, 'soft', 'Type of limits used in homing: soft or hard']
+    ]
+    
+#    result_def = [
+#        ['homed',  Type.Bool, None, 'Motor homed state']
+#    ]
+
+    RIGHT_MOT = 'oh_diag_right'
+    LEFT_MOT = 'oh_diag_left'
+    
+    RIGHT_HOMING_DIR = -1
+    LEFT_HOMING_DIR = 1
+
+    GROUP_STRICT = False
+
+    def prepare(self, *args, **opts):
+        self.limits = args[0].lower()
+        if not self.limits in ('soft','hard'):
+            self.error("""limits parameter is expected to be 'soft' or 'hard'""")
+            return False
+        right_mot_pos = PyTango.AttributeProxy(self.RIGHT_MOT + '/position')
+        left_mot_pos = PyTango.AttributeProxy(self.LEFT_MOT + '/position')
+        right_mot_pos_conf = right_mot_pos.get_config()
+        left_mot_pos_conf = left_mot_pos.get_config()
+        
+        #here we mask software limits, to allow reaching hardware limits
+        if self.limits == 'hard': 
+            self.debug('Masking software limits...')
+            self.old_top_mot_max = right_mot_pos_conf.max_value
+            self.old_bottom_mot_min = left_mot_pos_conf.min_value
+            right_mot_pos_conf.max_value = '9.999E+003'
+            left_mot_pos_conf.min_value = '-9.999E+003'
+            right_mot_pos.set_config(right_mot_pos_conf)
+            left_mot_pos.set_config(left_mot_pos_conf)
+        
+        self.top_mot_max = right_mot_pos_conf.max_value
+        self.bottom_mot_min = left_mot_pos_conf.min_value     
+        self.debug("Right motor max position: %s" % self.top_mot_max)
+        self.debug("Left motor min position: %s" % self.bottom_mot_min)
+
+    def run(self, *args, **opts):        
+        #this motion is separeted because of limit activation which could breake a two motors simultaneous motion
+        try:
+	    pass
+            self.execMacro('mv', self.RIGHT_MOT, self.top_mot_max)
+            self.execMacro('mv', self.LEFT_MOT, self.bottom_mot_min)
+            return self.execMacro('ipap_homing', self.GROUP_STRICT, self.RIGHT_MOT, self.RIGHT_HOMING_DIR, self.LEFT_MOT, self.LEFT_HOMING_DIR)
+        except:
+            return False
+        finally:
+            if self.limits == 'hard':
+                self.debug('Unmasking software limits...')
+                right_mot_pos = PyTango.AttributeProxy(self.RIGHT_MOT + '/position')
+                left_mot_pos = PyTango.AttributeProxy(self.LEFT_MOT + '/position')
+                right_mot_pos_conf = right_mot_pos.get_config()
+                left_mot_pos_conf = left_mot_pos.get_config()
+                right_mot_pos_conf.max_value = self.old_top_mot_max
+                left_mot_pos_conf.min_value = self.old_bottom_mot_min
+                right_mot_pos.set_config(right_mot_pos_conf)
+                left_mot_pos.set_config(left_mot_pos_conf)
+        
+        #self.debug("Right motor max position: %s" % right_mot_pos_conf.max_value)
+        #self.debug("Left motor min position: %s" % left_mot_pos_conf.min_value)
+
+    def on_abort(self):
+        pass
+
 
 class diag_homing_foil(Macro):
     """ 
