fe3b6af6fffad43004979dc52a0da396d40216b6
zreszela <zreszela@users.sourceforge.net>
2011-06-10 10:34:16 +0000

    constKscan added
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@3623 381f683f-204f-49a0-b5f1-b96b46cb89f0

--- MacroServer/macros/ALBA_BL22_CLAESS/_scan.py
+++ MacroServer/macros/ALBA_BL22_CLAESS/_scan.py
@@ -1,6 +1,94 @@
-from scan import ascan
-from macro import Type, Macro
+from scan import SScan, ascan, getCallable, UNCONSTRAINED
+from macro import Type, Macro, Hookable
 import math
+
+EV2REVA = 0.2624682843
+
+class constKscan(Macro, Hookable):
+    """"""
+
+    param_def = [
+       ['motor',            Type.Motor,   None, 'Motor to move'],
+       ['start_pos',        Type.Float,   None, 'Scan start position'],
+       ['final_pos',        Type.Float,   None, 'Scan final position'],
+       ['edge_pos',         Type.Float,   None, 'Edge position'],
+       ['step_size_k',      Type.Float,   None, 'Scan step size k'],
+       ['start_integ_time', Type.Float,   None, 'Start integration time'],
+       ['pow_integ_time',   Type.Float,   None, 'Power integration time']
+    ]
+    
+    def __calc_pos(self, i, edge_pos, step_size_k):
+        pos = edge_pos + (i * step_size_k) ** 2 / EV2REVA
+        return pos
+
+    def prepare(self, *args, **opts):                        
+        motor = args[0]
+        start_pos = args[1]
+        final_pos = args[2]
+        edge_pos = args[3]
+        step_size_k = args[4]
+
+        #precalculate positions to get know nr of scan points
+        self.nr_points,i = 0,1
+        while True:
+            pos = self.__calc_pos(i,edge_pos,step_size_k)
+            i += 1
+            if pos < start_pos:
+                continue
+            elif pos > final_pos:
+                break
+            self.nr_points += 1
+
+        generator=self._generator
+        moveables=[motor]
+        env=opts.get('env',{})
+        constrains=[getCallable(cns) for cns in opts.get('constrains',[UNCONSTRAINED])]
+        
+        self.pre_scan_hooks = self.getHooks('pre-scan')
+        self.post_scan_hooks = self.getHooks('post-scan')
+        self._gScan=SScan(self, generator, moveables, env, constrains)
+        
+    def _generator(self):
+        args = self.getParameters()
+        start_pos = args[1]
+        final_pos = args[2]
+        edge_pos = args[3]
+        step_size_k = args[4]
+        start_integ_time = args[5]
+        pow_integ_time = args[6]
+
+        step = {}
+        step["pre-move-hooks"] = self.getHooks('pre-move')
+        step["post-move-hooks"] = self.getHooks('post-move')
+        step["pre-acq-hooks"] = self.getHooks('pre-acq')
+        step["post-acq-hooks"] = self.getHooks('post-acq') + self.getHooks('_NOHINTS_')
+        step["post-step-hooks"] = self.getHooks('post-step')
+        
+        step["check_func"] = []
+
+        point_id,i = 0,1
+        while True:
+            pos = self.__calc_pos(i,edge_pos,step_size_k)
+            i += 1
+            if pos < start_pos:
+                continue
+            elif pos > final_pos:
+                break
+            if point_id == 0: first_pos = pos
+            t = start_integ_time * ((pos - edge_pos)/(first_pos - edge_pos)) ** (pow_integ_time * 0.5)
+            step["positions"] = [pos]
+            step["integ_time"] = t
+            step["point_id"] = point_id
+            yield step
+            point_id += 1
+    
+    def run(self,*args):
+        for step in self._gScan.step_scan():
+            yield step
+
+    @property
+    def data(self):
+        return self._gScan.data
 
 class rscan(ascan): 
     """Do an absolute scan of the specified motor.
