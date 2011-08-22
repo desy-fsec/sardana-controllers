38efeab059517be6dc39b1b81dfb4594c87da71c
zreszela <zreszela@users.sourceforge.net>
2011-08-22 14:15:27 +0000

    ExitOffsetPseudomotorController moved to its own module
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@3843 381f683f-204f-49a0-b5f1-b96b46cb89f0

--- Pool/ctrls/lib/python/pseudomotor/ALBA_BL22_CLAESS/BL22_CLAESS_DCM_pseudomotorcontroller.py
+++ Pool/ctrls/lib/python/pseudomotor/ALBA_BL22_CLAESS/BL22_CLAESS_DCM_pseudomotorcontroller.py
@@ -19,46 +19,6 @@ def siliconBondLength(ior_value):
     else:
         raise ValueError("Wrong ior value")
 
-class DCM_ExitOffset_Controller(PseudoMotorController):
-    """This pseudomotor controller does the calculation of Bragg
-    and 2nd Xtal perpendicular position according to desired energy in eV.
-    It works only with ClaesDcmTurboPmacCtrl motors"""
-    
-    pseudo_motor_roles = ('exitOffset',)
-    motor_roles = ('perp',)
-                             
-    class_prop = { 'DCMBraggName':{'Type' : 'PyTango.DevString', 'Description' : 'DCM bragg motor name'}}
-
-    def __init__(self, inst, props):    
-        PseudoMotorController.__init__(self, inst, props)
-        #self._log.setLevel(logging.DEBUG)
-
-        try:
-            self.dcm_bragg = PoolUtil().get_motor(self.inst_name, self.DCMBraggName)
-        except Exception, e:
-            self._log.error("Couldn't create DeviceProxy for %s motor." % self.DCMBraggName)
-            raise e
-        
-    def calc_physical(self, index, pseudos):
-        return self.calc_all_physical(pseudos)[index - 1]
-    
-    def calc_pseudo(self, index, physicals):
-        return self.calc_all_pseudo(physicals)[index - 1]
-    
-    def calc_all_physical(self, pseudos):
-        exitOffset, = pseudos
-        bragg_deg = self.dcm_bragg.position
-        bragg_rad = math.radians(bragg_deg)
-        perp =exitOffset/2/math.cos(bragg_rad)
-        return (perp,)
-        
-    def calc_all_pseudo(self, physicals):
-        perp_mm, = physicals
-        bragg_deg = self.dcm_bragg.position
-        bragg_rad = math.radians(bragg_deg)
-        exitOffset = 2 * perp_mm * math.cos(bragg_rad)
-        return (exitOffset,)
-  
 class DCM_Energy_Controller(PseudoMotorController):
     """This pseudomotor controller does the calculation of Bragg
     and 2nd Xtal perpendicular position according to desired energy in eV.
