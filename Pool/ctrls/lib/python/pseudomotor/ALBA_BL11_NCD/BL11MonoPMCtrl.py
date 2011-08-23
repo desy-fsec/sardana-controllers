c27a2889a316db05b8af42ebda202ac3a9c92001
sergiblanch <sergiblanch@users.sourceforge.net>
2011-08-23 15:15:49 +0000

    introduced the calculations from physicals to energy an viceversa also introduced a test to compare with manufacturer's table and it complains
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@3863 381f683f-204f-49a0-b5f1-b96b46cb89f0

--- Pool/ctrls/lib/python/pseudomotor/ALBA_BL11_NCD/BL11MonoPMCtrl.py
+++ Pool/ctrls/lib/python/pseudomotor/ALBA_BL11_NCD/BL11MonoPMCtrl.py
@@ -31,13 +31,26 @@
 ## along with this program; if not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
 
-import math
+from math import sin,degrees,radians
 import PyTango
 from pool import PseudoMotorController
 
+import bragg #FIXME: this comes from ALBA repository, needs to be shared
+#TODO: generalize to more crystals than ncds'
+
+Delta = 0.030 #m gap between crystals
+NaN = float('nan')
+
 class Bl11Energy(PseudoMotorController):
     """Energy pseudo motor controller for handling BL11-NCD energy calculation
-       given the positions of all the motors involved (and viceversa)."""
+       given the positions of all the motors involved (and viceversa).
+       From: andreas.schacht at accel.de
+       To: sergi.blanch at cells.es
+       Date: 2009/03/24 15:00
+       Subject: AW: documentation appendix of ranges and translations
+       T2=d*sin(theta)/sin(2*theta)
+       d is the offset (in your case 30 mm) and theta the Bragg angle in degrees
+    """
     gender = "Energy"
     model  = "BL11-NCD energy pseudomotor"
     organization = "CELLS - ALBA"
@@ -56,7 +69,57 @@ class Bl11Energy(PseudoMotorController):
         self.debug = False
 
     def calc_physical(self, index, pseudos):
-        return 0
+        return self.calc_all_physical(pseudos)[index - 1]
 
     def calc_pseudo(self, index, physicals):
-        return 0
+        return self.calc_all_pseudo(physicals)[index - 1]
+
+    def calc_all_physical(self, pseudos):
+        #bragg angle is called theta to distinguish with the bragg library
+        energy = pseudos
+        try:
+            #bragg.ener2angle(E, d=None, n=1 means Si_111)
+            theta_radians = bragg.ener2angle(bragg.joules(energy*1000))
+            theta = degrees(theta_radians)
+            t2 = ((Delta*sin(theta_radians))/sin(2*theta_radians))*1000
+            return (theta,t2)
+        except Exception,e:
+            self._log.error('calc_all_physical(): Exception %s'%(e))
+            return (NaN,NaN)
+
+    def calc_all_pseudo(self, physicals):
+        #bragg angle is called theta to distinguish with the bragg library
+        theta,t2 = physicals
+        try:
+            #bragg.angle2ener(scatangle, d=None, n=1):
+            return bragg.electronvolts(bragg.angle2ener(radians(theta)))/1000
+        except Exception,e:
+            self._log.error('calc_all_pseudo(): Exception %s'%(e))
+            return (NaN)
+
+if __name__ == '__main__':
+  # Just some unit tests of the computation
+  dcmTest = Bl11Energy('test_name',{})
+  dcmTest.name = 'test_name'
+  braggRange = [ 7.5,#e=15.1 keV
+                 8.0,#e=14.2 keV
+                 9.0,#e=12.6 keV
+                10.0,#e=11.4 keV
+                12.0,#e= 9.5 keV
+                15.0,#e= 7.6 keV
+                17.5,#e= 6.6 keV
+                20.0,#e= 5.8 keV
+                22.5,#e= 5.2 keV
+                25.0,#e= 4.7 keV
+                27.0,#e= 4.4 keV
+                28.0,#e= 4.2 keV
+                29.0,#e= 4.1 keV
+                29.5,#e= 4.0 keV
+                30.0]#e= 4.0 keV
+  t2Range = [20]
+  physicalsTest = [(i,j) for i in braggRange for j in t2Range]
+  for test in physicalsTest:
+      energy = dcmTest.calc_all_pseudo(test)
+      theta,t2 = dcmTest.calc_all_physical(energy)
+      print("bragg= %3.1f\tenergy= %3.1f\t theta=%3.1f\tt2=%4.2f"%(test[0],energy,theta,t2))
+
