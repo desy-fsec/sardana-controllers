dff2066c389e414dc3d33040757234b7d583d1bb
zreszela <zreszela@users.sourceforge.net>
2011-10-10 09:50:55 +0000

    vfm_homing_vert revised and tested
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@4088 381f683f-204f-49a0-b5f1-b96b46cb89f0

--- MacroServer/macros/ALBA_BL22_CLAESS/VFM.py
+++ MacroServer/macros/ALBA_BL22_CLAESS/VFM.py
@@ -2,27 +2,22 @@ import time
 import PyTango
 
 from macro import Macro, Type
-#from icepap import home_group_strict, home_group, create_motor_info_dict
-from macro_utils.motors import moveToPosHardLim, moveToNegHardLim
+from macro_utils.motors import moveToHardLim
 from macro_utils.icepap import *
             
 class vfm_homing_vert(Macro):
     """ 
-    This macro does the homing of vertical jacks BL22-CLAESS VFM(Vertical Focusing Mirror).
-    Homing procedure is done in 3 steps:
+    This macro does homing of vertical jacks of BL22-CLAESS VFM(Vertical Focusing Mirror).
+    Homing procedure is done in 2 steps:
 
-    1. Simultaneously move all the jacks to their positive limits(software or hardware)
+    1. Simultaneously move all the jacks to their positive limits.
        Whenever any of the jacks reaches its limit, simultaneous motion is continue with the rest of them. 
-    2. When all of them reached extreams Icepap homing routine is started (GROUP STRICT) for all of the jack
+    2. When all of them reach extreams Icepap homing routine is started (GROUP STRICT) for all of the jack
        simultaneously - in negative direction. Whenever any of the jacks is stopped by a STOP command, 
        a limit switch or an alarm condition, all the other axes in the group are forced to stop immediately.
 
     In case of successfully homing of all jacks macro returns True, in all other cases it return False
     """
-
-    param_def = [
-        ['limits', Type.String, 'soft', 'Type of limits used in homing: soft or hard']
-    ]
     
     result_def = [
         ['homed',  Type.Boolean, None, 'Motor homed state']
@@ -31,64 +26,74 @@ class vfm_homing_vert(Macro):
     JACK1_NAME = 'oh_vfm_jack1'
     JACK2_NAME = 'oh_vfm_jack2'
     JACK3_NAME = 'oh_vfm_jack3'
+    PITCH_NAME = 'oh_vfm_pitch'
+    ROLL_NAME  = 'oh_vfm_roll'
 
     JACK1_HOMING_DIR = -1
     JACK2_HOMING_DIR = -1
     JACK3_HOMING_DIR = -1
 
-    GROUP_STRICT = True
-
     def prepare(self, *args, **opts):
-        self.limits = args[0].lower()
-        if not self.limits in ('soft','hard'):
-            self.error("""limits parameter is expected to be 'soft' or 'hard'""")
-            return False
         
         self.jack1 = self.getObj(self.JACK1_NAME, type_class=Type.Motor)
         self.jack2 = self.getObj(self.JACK2_NAME, type_class=Type.Motor)
         self.jack3 = self.getObj(self.JACK3_NAME, type_class=Type.Motor)
 
+        pitch_pos = PyTango.AttributeProxy(self.PITCH_NAME + '/position')
+        roll_pos = PyTango.AttributeProxy(self.ROLL_NAME + '/position')
+
         jack1_pos = PyTango.AttributeProxy(self.JACK1_NAME + '/position')
         jack2_pos = PyTango.AttributeProxy(self.JACK2_NAME + '/position')
         jack3_pos = PyTango.AttributeProxy(self.JACK3_NAME + '/position')
         
+        pitch_pos_conf = pitch_pos.get_config()
+        roll_pos_conf = roll_pos.get_config()
+
         jack1_pos_conf = jack1_pos.get_config()
         jack2_pos_conf = jack2_pos.get_config()
         jack3_pos_conf = jack3_pos.get_config()
         
         #here we mask software limits, to allow reaching hardware limits
-        if self.limits == 'hard': 
-            self.debug('Masking software limits...')
-            self.old_jack1_max = jack1_pos_conf.max_value
-            self.old_jack2_max = jack2_pos_conf.max_value
-            self.old_jack3_max = jack3_pos_conf.max_value
-            jack1_pos_conf.max_value = '9.999E+003'
-            jack2_pos_conf.max_value = '9.999E+003'
-            jack3_pos_conf.max_value = '9.999E+003'
-            jack1_pos.set_config(jack1_pos_conf)
-            jack2_pos.set_config(jack2_pos_conf)
-            jack3_pos.set_config(jack3_pos_conf)
+        self.debug('Masking software limits...')
+        self.old_jack1_max = jack1_pos_conf.max_value
+        self.old_jack2_max = jack2_pos_conf.max_value
+        self.old_jack3_max = jack3_pos_conf.max_value
+        self.old_pitch_max = pitch_pos_conf.max_value
+        self.old_pitch_min = pitch_pos_conf.min_value
+        self.old_roll_max = roll_pos_conf.max_value
+        self.old_roll_min = roll_pos_conf.min_value
         
+        jack1_pos_conf.max_value = '9.999E+003'
+        jack2_pos_conf.max_value = '9.999E+003'
+        jack3_pos_conf.max_value = '9.999E+003'
+        
+        pitch_pos_conf.max_value = '9.999E+003'
+        pitch_pos_conf.min_value = '-9.999E+003'
+        roll_pos_conf.max_value = '9.999E+003'
+        roll_pos_conf.min_value = '-9.999E+003'
+        
+        jack1_pos.set_config(jack1_pos_conf)
+        jack2_pos.set_config(jack2_pos_conf)
+        jack3_pos.set_config(jack3_pos_conf)
+        pitch_pos.set_config(pitch_pos_conf)
+        roll_pos.set_config(roll_pos_conf)
+
         self.jack1_max = jack1_pos_conf.max_value
         self.jack2_max = jack2_pos_conf.max_value
         self.jack3_max = jack3_pos_conf.max_value
         
-        self.debug("Jack1 motor max position: %s" % self.jack1_max)
-        self.debug("Jack2 motor max position: %s" % self.jack2_max)
-        self.debug("Jack3 motor max position: %s" % self.jack3_max)
-
     def run(self, *args, **opts):        
         try:
-            motors_pos_dict = {self.jack1:float(self.jack1_max), self.jack2:float(self.jack2_max), self.jack3:float(self.jack3_max)}
-            while len(motors_pos_dict):
-                motorsOnPosLim = moveToPosHardLim(self, motors_pos_dict)
-                for m in motorsOnPosLim:
-                    motors_pos_dict.pop(m)
+            
+            info_dict = {self.JACK1_NAME:(self.jack1, 999, 1), self.JACK2_NAME:(self.jack2, 999, 1), self.JACK3_NAME:(self.jack3, 999, 1)}
+            while len(info_dict):
+                motorsOnLim = moveToHardLim(self, info_dict.values())
+                for mot in motorsOnLim:
+                    info_dict.pop(mot)
             jack1_info = create_motor_info_dict(self.jack1, self.JACK1_HOMING_DIR)
             jack2_info = create_motor_info_dict(self.jack2, self.JACK2_HOMING_DIR)
             jack3_info = create_motor_info_dict(self.jack3, self.JACK3_HOMING_DIR)
             
-            self.aborted = False
             res = home_group_strict(self, [jack1_info, jack2_info, jack3_info])
             if res and jack2_info['homed']:
                 self.debug('Motor %s successfully homed.' % jack2_info['motor'].alias())
@@ -113,48 +118,48 @@ class vfm_homing_vert(Macro):
                 self.error('VCM vertical jacks homing failed.')  
                 return False
         except Exception, e:
-            #@TODO: abort all motion
             self.error(repr(e))
             raise e
         finally:
-            if self.limits == 'hard':
-                self.debug('Unmasking software limits...')
-                jack1_pos = PyTango.AttributeProxy(self.JACK1_NAME + '/position')
-                jack2_pos = PyTango.AttributeProxy(self.JACK2_NAME + '/position')
-                jack3_pos = PyTango.AttributeProxy(self.JACK3_NAME + '/position')
-                jack1_pos_conf = jack1_pos.get_config()
-                jack2_pos_conf = jack2_pos.get_config()
-                jack3_pos_conf = jack3_pos.get_config()
-                jack1_pos_conf.max_value = self.old_jack1_max
-                jack2_pos_conf.max_value = self.old_jack2_max
-                jack3_pos_conf.max_value = self.old_jack3_max
-                jack1_pos.set_config(jack1_pos_conf)
-                jack2_pos.set_config(jack2_pos_conf)
-                jack3_pos.set_config(jack3_pos_conf)
-        
-        #self.debug("Top motor max position: %s" % jack1_pos_conf.max_value)
-        #self.debug("Bottom motor min position: %s" % bottom_mot_pos_conf.min_value)
+            #@TODO: Uncomment below line if the serialization problem will be fixed
+            #self.debug('Unmasking software limits...')
+            jack1_pos = PyTango.AttributeProxy(self.JACK1_NAME + '/position')
+            jack2_pos = PyTango.AttributeProxy(self.JACK2_NAME + '/position')
+            jack3_pos = PyTango.AttributeProxy(self.JACK3_NAME + '/position')
+            pitch_pos = PyTango.AttributeProxy(self.PITCH_NAME + '/position')
+            roll_pos = PyTango.AttributeProxy(self.ROLL_NAME + '/position')
+            jack1_pos_conf = jack1_pos.get_config()
+            jack2_pos_conf = jack2_pos.get_config()
+            jack3_pos_conf = jack3_pos.get_config()
+            pitch_pos_conf = pitch_pos.get_config()
+            roll_pos_conf = roll_pos.get_config()
+            jack1_pos_conf.max_value = self.old_jack1_max
+            jack2_pos_conf.max_value = self.old_jack2_max
+            jack3_pos_conf.max_value = self.old_jack3_max
+            pitch_pos_conf.max_value = self.old_pitch_max
+            pitch_pos_conf.min_value = self.old_pitch_min
+            roll_pos_conf.max_value = self.old_roll_max
+            roll_pos_conf.min_value = self.old_roll_min
+            jack1_pos.set_config(jack1_pos_conf)
+            jack2_pos.set_config(jack2_pos_conf)
+            jack3_pos.set_config(jack3_pos_conf)
+            pitch_pos.set_config(pitch_pos_conf)
+            roll_pos.set_config(roll_pos_conf)
 
-    def on_abort(self):
-        self.aborted = True
-
+    
 class vfm_homing_hori(Macro):
     """ 
-    This macro does the homing of vertical jacks BL22-CLAESS VFM(Vertical Focusing Mirror).
+    This macro does the homing of horizontal translations ofBL22-CLAESS VFM(Vertical Focusing Mirror).
     Homing procedure is done in 3 steps:
 
-    1. Simultaneously move all the jacks to their positive limits(software or hardware)
-       Whenever any of the jacks reaches its limit, simultaneous motion is continue with the rest of them. 
-    2. When all of them reached extreams Icepap homing routine is started (GROUP STRICT) for all of the jack
-       simultaneously - in negative direction. Whenever any of the jacks is stopped by a STOP command, 
+    1. Simultaneously move all the translations to their negative hardware limits.
+       Whenever any of the translations reaches its limit, simultaneous motion is continue with the second one. 
+    2. When all of them reached extreams Icepap homing routine is started (GROUP) for both translations
+       simultaneously - in positive direction. Whenever any of the jacks is stopped by a STOP command, 
        a limit switch or an alarm condition, all the other axes in the group are forced to stop immediately.
 
-    In case of successfully homing of all jacks macro returns True, in all other cases it return False
+    In case of successfully homing of both translations macro returns True, in all other cases it return False.
     """
-
-    param_def = [
-        ['limits', Type.String, 'soft', 'Type of limits used in homing: soft or hard']
-    ]
     
     result_def = [
         ['homed',  Type.Boolean, None, 'Motor homed state']
@@ -162,80 +167,77 @@ class vfm_homing_hori(Macro):
 
     X1_NAME = 'oh_vfm_x1'
     X2_NAME = 'oh_vfm_x2'
+    YAW_NAME = 'oh_vfm_yaw'
 
     X1_HOMING_DIR = 1
     X2_HOMING_DIR = 1
 
-    GROUP_STRICT = True
 
     def prepare(self, *args, **opts):
-        self.limits = args[0].lower()
-        if not self.limits in ('soft','hard'):
-            self.error("""limits parameter is expected to be 'soft' or 'hard'""")
-            return False
         
         self.x1 = self.getObj(self.X1_NAME, type_class=Type.Motor)
         self.x2 = self.getObj(self.X2_NAME, type_class=Type.Motor)
 
         x1_pos = PyTango.AttributeProxy(self.X1_NAME + '/position')
         x2_pos = PyTango.AttributeProxy(self.X2_NAME + '/position')
+        yaw_pos = PyTango.AttributeProxy(self.YAW_NAME + '/position')
         
         x1_pos_conf = x1_pos.get_config()
         x2_pos_conf = x2_pos.get_config()
+        yaw_pos_conf = yaw_pos.get_config() 
 
         #here we mask software limits, to allow reaching hardware limits
-        if self.limits == 'hard': 
-            self.debug('Masking software limits...')
-            self.old_x1_min = x1_pos_conf.min_value
-            self.old_x2_min = x2_pos_conf.min_value
-            x1_pos_conf.min_value = '-9.999E+003'
-            x2_pos_conf.min_value = '-9.999E+003'
-            x1_pos.set_config(x1_pos_conf)
-            x2_pos.set_config(x2_pos_conf)
-        
-        self.x1_min = x1_pos_conf.min_value
-        self.x2_min = x2_pos_conf.min_value
-        
-        self.debug("X1 motor min position: %s" % self.x1_min)
-        self.debug("X2 motor min position: %s" % self.x2_min)
+        self.debug('Masking software limits...')
+        self.old_x1_min = x1_pos_conf.min_value
+        self.old_x2_min = x2_pos_conf.min_value
+        self.old_yaw_min = yaw_pos_conf.min_value
+        self.old_yaw_max = yaw_pos_conf.max_value
+        x1_pos_conf.min_value = '-9.999E+003'
+        x2_pos_conf.min_value = '-9.999E+003'
+        yaw_pos_conf.min_value = '-9.999E+003'
+        yaw_pos_conf.max_value = '9.999E+003'
+        x1_pos.set_config(x1_pos_conf)
+        x2_pos.set_config(x2_pos_conf)
+        yaw_pos.set_config(yaw_pos_conf)
 
     def run(self, *args, **opts):        
         try:
-            motors_pos_dict = {self.x1:float(self.x1_min), self.x2:float(self.x2_min)}
-            while len(motors_pos_dict):
-                motorsOnNegLim = moveToNegHardLim(self, motors_pos_dict)
-                for m in motorsOnNegLim:
-                    motors_pos_dict.pop(m)
+            info_dict = {self.X1_NAME:(self.x1, -999, -1), self.X2_NAME:(self.x2, -999, -1)}
+            while len(info_dict):
+                motorsOnLim = moveToHardLim(self, info_dict.values())
+                for mot in motorsOnLim:
+                    info_dict.pop(mot)
+
             x1_info = create_motor_info_dict(self.x1, self.X1_HOMING_DIR)
             x2_info = create_motor_info_dict(self.x2, self.X2_HOMING_DIR)
             
-            self.aborted = False
             res = home_group(self, [x1_info, x2_info])
-            if res:
-                self.debug('Motors successfully homed.')
+            if res == True:
+                self.debug('VFM lateral motors successfully homed.')
+                return True
+            else: 
+                self.debug('VFM lateral motors homing failed.')
                 return False
         except Exception, e:
-            #@TODO: abort all motion
             self.error(repr(e))
             raise e
-        finally:
-            if self.limits == 'hard':
-                self.debug('Unmasking software limits...')
-                x1_pos = PyTango.AttributeProxy(self.X1_NAME + '/position')
-                x2_pos = PyTango.AttributeProxy(self.X2_NAME + '/position')
-                x1_pos_conf = x1_pos.get_config()
-                x2_pos_conf = x2_pos.get_config()
-                x1_pos_conf.min_value = self.old_x1_min
-                x2_pos_conf.min_value = self.old_x2_min
-                x1_pos.set_config(x1_pos_conf)
-                x2_pos.set_config(x2_pos_conf)
+        finally:    
+            #@TODO: Uncomment below line if the serialization problem will be fixed
+            #self.debug('Unmasking software limits...')
+            x1_pos = PyTango.AttributeProxy(self.X1_NAME + '/position')
+            x2_pos = PyTango.AttributeProxy(self.X2_NAME + '/position')
+            yaw_pos = PyTango.AttributeProxy(self.YAW_NAME + '/position')
+            x1_pos_conf = x1_pos.get_config()
+            x2_pos_conf = x2_pos.get_config()
+            yaw_pos_conf = yaw_pos.get_config()
+            x1_pos_conf.min_value = self.old_x1_min
+            x2_pos_conf.min_value = self.old_x2_min
+            yaw_pos_conf.min_value = self.old_yaw_min
+            yaw_pos_conf.max_value = self.old_yaw_max
+            x1_pos.set_config(x1_pos_conf)
+            x2_pos.set_config(x2_pos_conf)
+            yaw_pos.set_config(yaw_pos_conf)
         
-        #self.debug("Top motor max position: %s" % x1_pos_conf.max_value)
-        #self.debug("Bottom motor min position: %s" % bottom_mot_pos_conf.min_value)
-
-    def on_abort(self):
-        self.aborted = True
-
 
 class vfm_homing_bend(Macro):
     """ 
