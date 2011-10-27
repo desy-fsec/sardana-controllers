f90ff5a9ec8040bc9e0135392b6d347bf6625a47
zreszela <zreszela@users.sourceforge.net>
2011-10-27 17:20:31 +0000

    added macro description
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@4150 381f683f-204f-49a0-b5f1-b96b46cb89f0

--- MacroServer/macros/ALBA_BL04_MSPD/COLL.py
+++ MacroServer/macros/ALBA_BL04_MSPD/COLL.py
@@ -6,16 +6,11 @@ from macro_utils.icepap import *
 from macro_utils.motors import moveToHardLim
 
 class coll_homing(Macro):
+    
     """ 
-    This macro does the homing of vertical slits of the BL22-CLAESS Diagnostic Module.
-    Homing procedure is done in 3 steps:
-    0. Software limits are masked. 
-    1. Top blade is moved to its positive harware limit and bottom blade is moved to its negative hardware limit (simultaneously).
-    3. Icepap homing procedure is started looking for home in the following directions:
-       - top blade moving negative
-       - bottom blade moving positive
-    4. When top blade finds its home, it is stopped and bottom continues in the same direction as before.
-    In case of successfully homing of both blades macro returns True, on all other cases it return False
+    This macro does homing of vertical motor of 2nd coll (BL04-MSPD).
+    It will start looking for homing position into positive direction.
+    In case of successfully homing macro returns True, in all other cases it return False.
     """
     
     result_def = [
