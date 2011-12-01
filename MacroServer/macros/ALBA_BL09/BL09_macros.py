78c390384820225cc8dcca3e0aeb645c23465a8b
anmilan <anmilan@users.sourceforge.net>
2011-12-01 14:10:32 +0000

    First commit of BL09 macros
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@4444 381f683f-204f-49a0-b5f1-b96b46cb89f0

---
+++ MacroServer/macros/ALBA_BL09/BL09_macros.py
@@ -0,0 +1,45 @@
+"""
+    Macro for testing the Movable Masks in FE09
+"""
+
+from macro import *
+import time
+
+class scanMM(Macro):
+    param_def = [ [ 'motor1', Type.Motor, None, 'Motor 1' ],
+                  [ 'startPos', Type.Float, None, 'startPos1' ],
+                  [ 'endPos', Type.Float, None, 'endPos1' ],
+                  [ 'nIntervals', Type.Integer, None, 'Number of intervals' ],
+                  [ 'integrationTime', Type.Float, None, 'Integration time' ],
+                  [ 'motor2', Type.Motor, None, 'Motor 2' ],
+                  [ 'fixedPos', Type.Float, None, 'Fixed Pos' ],
+                  [ 'sleepTime', Type.Float, None, 'sleep time' ],
+                ]
+
+    def prepare(self, motor1,startPos,endPos,nIntervals,integrationTime,motor2,fixedPos,sleepTime):
+        """Check that parameters for the macro are correct"""
+        self.output("\n\n~~~~~ Preparing scanMM macro ~~~~")
+        self.motor2 = motor2
+        self.sleepTime = sleepTime
+        self.fixedPos = fixedPos
+        
+    def myHook(self):
+        self.motor2.move(self.fixedPos)
+        time.sleep(self.sleepTime)
+    
+    def run(self, motor1,startPos,endPos,nIntervals,integrationTime,motor2,fixedPos,sleepTime):
+        #"""Run macro"""       
+        myMacro, pars = self.createMacro("ascan",motor1,startPos,endPos,nIntervals,integrationTime)
+        #myMacro, pars = self.createMacro("ascan",'BL24_dummymotor01',0,10,5,0.1)
+        myMacro.hooks = [self.myHook]
+        self.runMacro(myMacro)
+
+#        stepsToMove = (startPos - endPos) / nIntervals
+#        
+#        for posToMove in xrange(startPos, endPos, nIntervals):
+#            #posToMove = startPos + stepsToMove * (interval+1)
+#            motor1.move(posToMove)
+#            time.sleep(sleepTime)
+#            motor2.move(fixedPos)
+#            time.sleep(sleepTime)
+#    
