ecd5c752335d6e3222afeec9af835f0d7bfbaa5f
zreszela <zreszela@users.sourceforge.net>
2012-02-04 12:25:39 +0000

    first version of xbpm pseudo counter
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/Sardana/trunk@4684 381f683f-204f-49a0-b5f1-b96b46cb89f0

---
+++ Pool/ctrls/lib/python/pseudocounter/ALBA_BL04_MSPD/AlbaBl04XBPMPCCtrl.py
@@ -0,0 +1,43 @@
+from pool import PseudoCounterController
+
+class AlbaBl04XBPMPCController(PseudoCounterController):
+    
+    ctrl_extra_attributes = {"Offset":{'Type':'PyTango.DevDouble','R/W Type':'PyTango.READ_WRITE'}}
+    
+    counter_roles = ('ixbt', 'ixbb', 'ixbr', 'ixbl')
+    pseudo_counter_roles = ('ixbz', 'ixbx', 'ixbtot')
+
+    def __init__(self, inst, props):
+        PseudoCounterController.__init__(self, inst, props)
+        self.extra_attributes = {1:{'offset':0.0},2:{'offset':0.0},3:{'offset':float('nan')}}
+
+    def pseudo_current(self, first, second):
+        return (first - second) / (first + second)
+
+    def calc(self, index, counter_values):
+        ixbt, ixbb, ixbr, ixbl = counter_values
+
+        if index == 1: #vertical
+            offset = self.extra_attributes[1]["offset"]
+            ixbz = (ixbt * offset - ixbb)/(ixbt * offset + ixbb)
+            return ixbz
+        elif index == 2: #horizontal
+            offset = self.extra_attributes[2]["offset"]
+            ixbx = (ixbr * offset - ixbl)/(ixbr * offset + ixbb)
+            return ixbx
+        elif index == 3:
+            ixbtot = (ixbt + ixbb + ixbr + ixbl) / 4
+            return ixbtot
+        
+    def GetExtraAttributePar(self, axis, name):
+        name = name.lower()
+        if name == 'offset':
+            return self.extra_attributes[axis]['offset']
+       
+    def SetExtraAttributePar(self, axis, name, value):
+        name = name.lower()
+        if name == 'offset':
+            if axis in [1,2]:
+                self.extra_attributes[axis]['offset'] = value
+            else:
+                raise Exception("Offset attribute is restricted only for 1 and 2 axes.")
