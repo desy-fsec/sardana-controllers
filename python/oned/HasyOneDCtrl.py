378bf4c36ced3596eb66af51c9962937173a64fb
tkracht <tkracht@users.sourceforge.net>
2013-02-07 09:29:00 +0000

    ...
    git-svn-id: file:///home/cpascual/src/sdnongit/svnbck/controllers/trunk@5557 381f683f-204f-49a0-b5f1-b96b46cb89f0

--- python/oned/HasyOneDCtrl.py
+++ python/oned/HasyOneDCtrl.py
@@ -1,55 +1,68 @@
 import PyTango
 from sardana.pool.controller import OneDController
-import time
+import time, os
+
+debugFlag = True
 
 class HasyOneDCtrl(OneDController):
     "This class is the Tango Sardana One D controller for Hasylab"
 
-    ctrl_extra_attributes = {'DataLength':{'Type':'PyTango.DevLong','R/W Type':'PyTango.READ_WRITE'}}
-			     
+    ctrl_extra_attributes = {'DataLength':{'Type':'PyTango.DevLong','R/W Type':'PyTango.READ_WRITE'},
+                             'TangoDevice':{'Type':'PyTango.DevString','R/W Type':'PyTango.READ_ONLY'},
+                             }
+                 
     class_prop = {'RootDeviceName':{'Type':'PyTango.DevString','Description':'The root name of the MCA Tango devices'}}
-			     
+                 
     MaxDevice = 97
 
     def __init__(self,inst,props, *args, **kwargs):
         OneDController.__init__(self,inst,props, *args, **kwargs)
-        print "PYTHON -> OneDController ctor for instance",inst
-
+        self.debugFlag = False
+        if os.isatty(1): 
+            self.debugFlag = True
+        if self.debugFlag: print "HasyOneDCtrl.__init__, inst ",self.inst_name,"RootDeviceName",self.RootDeviceName
         self.ct_name = "HasyOneDCtrl/" + self.inst_name
         self.db = PyTango.Database()
         name_dev_ask =  self.RootDeviceName + "*"
-	self.devices = self.db.get_device_exported(name_dev_ask)
+        self.devices = self.db.get_device_exported(name_dev_ask)
         self.max_device = 0
         self.tango_device = []
         self.proxy = []
+        self.flagIsMCA8715 = []
         self.device_available = []
-	for name in self.devices.value_string:
+        for name in self.devices.value_string:
             self.tango_device.append(name)
             self.proxy.append(None)
-            self.device_available.append(0)
+            self.flagIsMCA8715.append( False)
+            self.device_available.append(False)
             self.max_device =  self.max_device + 1
         self.started = False
         self.dft_DataLength = 0
         self.DataLength = []
+
         
     def AddDevice(self,ind):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In AddDevice method for index",ind
         OneDController.AddDevice(self,ind)
         if ind > self.max_device:
-            print "False index"
+            print "HasyOneDCtrl: False index %d max %d" % (ind, self.max_device)
             return
         self.proxy[ind-1] = PyTango.DeviceProxy(self.tango_device[ind-1])
-        self.device_available[ind-1] = 1
+        self.device_available[ind-1] = True
         self.DataLength.append(self.dft_DataLength)
-        
+        if hasattr(self.proxy[ind-1], 'BankId'):
+            self.flagIsMCA8715[ind-1] = True
+        if self.debugFlag: print "HasyOneDCtrl.AddDevice ",self.inst_name,"index",ind, "isMCA8715", self.flagIsMCA8715[ind-1]
+
+       
     def DeleteDevice(self,ind):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In DeleteDevice method for index",ind
+        if self.debugFlag: print "HasyOneDCtrl.DeleteDevice",self.inst_name,"index",ind
         OneDController.DeleteDevice(self,ind)
         self.proxy[ind-1] =  None
         self.device_available[ind-1] = 0
         
+        
     def StateOne(self,ind):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In StateOne method for index",ind
+        if self.debugFlag: print "HasyOneDCtrl.StatOne",self.inst_name,"index",ind
         if  self.device_available[ind-1] == 1:
             sta = self.proxy[ind-1].command_inout("State")
             if sta == PyTango.DevState.ON:
@@ -61,6 +74,7 @@ class HasyOneDCtrl(OneDController):
         return tup
     
     def LoadOne(self, axis, value):
+        if self.debugFlag: print "HasyOneDCtrl.LoadOne",self.inst_name,"axis", axis
         idx = axis - 1
         if value > 0:
             self.integ_time = value
@@ -71,69 +85,74 @@ class HasyOneDCtrl(OneDController):
         
 
     def PreReadAll(self):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In PreReadAll method"
+        if self.debugFlag: print "HasyOneDCtrl.PreReadAll",self.inst_name
         pass
 
     def PreReadOne(self,ind):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In PreReadOne method for index",ind
+        if self.debugFlag: print "HasyOneDCtrl.PreReadOne",self.inst_name,"index",ind
+        if self.proxy[ind-1].state() != PyTango.DevState.ON:
+            self.proxy[ind-1].command_inout("Stop")
         self.proxy[ind-1].command_inout("Read")
 
     def ReadAll(self):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In ReadAll method"
+        if self.debugFlag: print "HasyOneDCtrl.ReadAll",self.inst_name
         pass
 
     def ReadOne(self,ind):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In ReadOne method for index",ind
-        return self.proxy[ind-1].Data
+        if self.debugFlag: print "HasyOneDCtrl.ReadOne",self.inst_name,"index",ind
+        data = self.proxy[ind-1].Data
+        return data
 
     def PreStartAll(self):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In PreStartAll method"
+        if self.debugFlag: print "HasyOneDCtrl.PreStartAll",self.inst_name
         pass
 
     def PreStartOne(self,ind, value):
         return True
-		
+        
     def StartOne(self,ind, value):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In StartOne method for index",ind
+        if self.debugFlag: print "HasyOneDCtrl.StartOne",self.inst_name,"index",ind
+        if self.flagIsMCA8715[ind-1]:
+            self.proxy[ind-1].BankId = 0
+        if self.proxy[ind-1].state() != PyTango.DevState.ON:
+            self.proxy[ind-1].command_inout("Stop")
         self.proxy[ind-1].command_inout("Clear")
         self.proxy[ind-1].command_inout("Start")
-		
+        
     def AbortOne(self,ind):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In AbortOne method for index",ind
+        if self.debugFlag: print "HasyOneDCtrl.AbortOne",self.inst_name,"index",ind
         self.proxy[ind-1].command_inout("Stop")
        
     def GetPar(self, ind, par_name):
-        if par_name == "datalength":
+        if self.debugFlag: print "HasyOneDCtrl.GetPar",self.inst_name,"index",ind, "par_name", par_name
+        if par_name == "DataLength": 
             if self.device_available[ind-1]:
-                print self.proxy[ind-1].read_attribute("DataLength").value
                 return int(self.proxy[ind-1].read_attribute("DataLength").value)
 
     def SetPar(self,ind,par_name,value):
         if par_name == "datalength":
             if self.device_available[ind-1]:
                 self.proxy[ind-1].write_attribute("DataLength",value)
-	
+    
     def GetExtraAttributePar(self,ind,name):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In GetExtraFeaturePar method for index",ind," name=",name
-        if name == "BankId":
+        if self.debugFlag: print "HasyOneDCtrl.GetExtraAttrPar",self.inst_name,"index",ind, "name", name
+        if name == "TangoDevice":
             if self.device_available[ind-1]:
-                return self.proxy[ind-1].read_attribute("BankId").value
+                return self.proxy[ind-1].name()
+        elif name == "DataLength": 
+            if self.device_available[ind-1]:
+                return int(self.proxy[ind-1].read_attribute("DataLength").value)
 
     def SetExtraAttributePar(self,ind,name,value):
-#        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": In SetExtraFeaturePar method for index",ind," name=",name," value=",value
-        if name == "BankId":
-            if self.device_available[ind-1]:
-                self.proxy[ind-1].write_attribute("BankId",value)
-        if name == "Clear":
-            if self.device_available[ind-1]:
-                self.proxy[ind-1].command_inout("Clear")
-        
+        if self.debugFlag: print "HasyOneDCtrl.SetExtraAttributePar",self.inst_name,"index",ind," name=",name," value=",value
+        pass
+
     def SendToCtrl(self,in_data):
-#        print "Received value =",in_data
+#        if self.debugFlag: print "Received value =",in_data
         return "Nothing sent"
         
     def __del__(self):
-        print "PYTHON -> HasyOneDCtrl/",self.inst_name,": Aarrrrrg, I am dying"
+        if self.debugFlag: print "HasyOneDCtrl/",self.inst_name,": Aarrrrrg, I am dying"
 
         
 if __name__ == "__main__":
