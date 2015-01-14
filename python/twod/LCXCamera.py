e9cd26dbaf46b44e3a41777385ccec6f77a9de9a
Teresa <tnunez@mail.desy.de>
2015-01-14 10:28:00 +0100

    Ctrl for lcxcamera

---
+++ python/twod/LCXCamera.py
@@ -0,0 +1,208 @@
+import PyTango
+import time, os
+
+from sardana import State, DataAccess
+from sardana.pool.controller import TwoDController
+from sardana.pool.controller import Type, Access, Description, DefaultValue
+from sardana.pool import PoolUtil
+
+ReadOnly = DataAccess.ReadOnly
+ReadWrite = DataAccess.ReadWrite
+
+class LCXCameraCtrl(TwoDController):
+    "This class is the Tango Sardana Two D controller for the LCXCamera"
+
+
+    ctrl_extra_attributes = {'DelayTime':{Type:'PyTango.DevDouble',Access:ReadWrite},
+			     'ExposureTime':{Type:'PyTango.DevDouble',Access:ReadWrite},
+			     'FileStartNum':{Type:'PyTango.DevLong',Access:ReadWrite},
+			     'FilePrefix':{Type:'PyTango.DevString',Access:ReadWrite},
+			     'FileDir':{Type:'PyTango.DevString',Access:ReadWrite},
+			     'NbFrames':{Type:'PyTango.DevLong',Access:ReadWrite},
+			     'Reset':{Type:'PyTango.DevLong',Access:ReadWrite},
+                             'TangoDevice':{Type:'PyTango.DevString',Access:ReadOnly}
+                             }
+			     
+    class_prop = {'RootDeviceName':{Type:str,Description:'The root name of the LCXCamera Tango devices'},
+                  'TangoHost':{Type:str,Description:'The tango host where searching the devices'},}
+			     
+    MaxDevice = 97
+
+    def __init__(self,inst,props, *args, **kwargs):
+        self.TangoHost = None
+        TwoDController.__init__(self,inst,props, *args, **kwargs)
+        print "PYTHON -> TwoDController ctor for instance",inst
+
+        self.ct_name = "LCXCameraCtrl/" + self.inst_name
+        if self.TangoHost == None:
+            self.db = PyTango.Database()
+        else:
+            self.node = self.TangoHost
+            self.port = 10000
+            if self.TangoHost.find( ':'):
+                lst = self.TangoHost.split(':')
+                self.node = lst[0]
+                self.port = int( lst[1])                           
+            self.db = PyTango.Database(self.node, self.port)
+        name_dev_ask =  self.RootDeviceName + "*"
+	self.devices = self.db.get_device_exported(name_dev_ask)
+        self.max_device = 0
+        self.tango_device = []
+        self.proxy = []
+        self.device_available = []
+	for name in self.devices.value_string:
+            self.tango_device.append(name)
+            self.proxy.append(None)
+            self.device_available.append(0)
+            self.max_device =  self.max_device + 1
+        self.started = False
+        self.dft_DelayTime = 0
+        self.DelayTime = []
+        self.dft_ExposureTime = 0
+        self.ExposureTime = []
+        self.dft_ExposurePeriod = 0
+        self.ExposurePeriod = []
+        self.dft_FileStartNum = 0
+        self.FileStartNum = []
+        self.dft_FilePrefix = ""
+        self.FilePrefix = []
+        self.dft_FileDir = ""
+        self.FileDir = []
+        self.dft_NbFrames = 0
+        self.NbFrames = []
+        self.dft_Reset = 0
+        self.Reset = []
+        
+        
+    def AddDevice(self,ind):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In AddDevice method for index",ind
+        TwoDController.AddDevice(self,ind)
+        if ind > self.max_device:
+            print "False index"
+            return
+        proxy_name = self.tango_device[ind-1]
+        if self.TangoHost == None:
+            proxy_name = self.tango_device[ind-1]
+        else:
+            proxy_name = str(self.node) + (":%s/" % self.port) + str(self.tango_device[ind-1])
+        self.proxy[ind-1] = PyTango.DeviceProxy(proxy_name)
+        self.device_available[ind-1] = 1
+        self.DelayTime.append(self.dft_DelayTime)
+        self.ExposureTime.append(self.dft_ExposureTime)
+        self.FileStartNum.append(self.dft_FileStartNum)
+        self.FilePrefix.append(self.dft_FilePrefix)
+        self.FileDir.append(self.dft_FileDir)
+        self.NbFrames.append(self.dft_NbFrames)
+        self.Reset.append(self.dft_Reset)
+        
+    def DeleteDevice(self,ind):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In DeleteDevice method for index",ind
+        TwoDController.DeleteDevice(self,ind)
+        self.proxy[ind-1] =  None
+        self.device_available[ind-1] = 0
+        
+    def StateOne(self,ind):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In StateOne method for index",ind
+        if  self.device_available[ind-1] == 1:
+            sta = self.proxy[ind-1].command_inout("State")
+            if sta == PyTango.DevState.ON:
+                tup = (sta,"Camera ready")
+            elif sta == PyTango.DevState.RUNNING:
+                sta = PyTango.DevState.MOVING
+                tup = (sta,"Camera taking images")
+            elif sta == PyTango.DevState.FAULT:
+                tup = (sta,"Camera in FAULT state")
+            elif sta == PyTango.DevState.DISABLE:
+                sta = PyTango.DevState.FAULT
+                tup = (sta,"Device disconnected from camserver")
+            return tup
+
+    def PreReadAll(self):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In PreReadAll method"
+        pass
+
+    def PreReadOne(self,ind):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In PreReadOne method for index",ind
+        pass
+
+    def ReadAll(self):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In ReadAll method"
+        pass
+
+    def ReadOne(self,ind):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In ReadOne method for index",ind
+        tmp_value = [(-1,), (-1,)]
+        if self.device_available[ind-1] == 1:
+            return tmp_value
+
+    def PreStartAll(self):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In PreStartAll method"
+        pass
+		
+    def StartOne(self,ind, position=None):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In StartOne method for index",ind
+        self.proxy[ind-1].command_inout("StartAcquisition")
+        
+    def AbortOne(self,ind):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In AbortOne method for index",ind
+        pass
+
+    def LoadOne(self, ind, value):
+        self.proxy[ind-1].write_attribute("ExposureTime",value)
+
+    def GetAxisPar(self, ind, par_name):
+        if par_name == "data_source":
+            data_source = "Not set"
+            return data_source
+ 
+    def GetExtraAttributePar(self,ind,name):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In GetExtraFeaturePar method for index",ind," name=",name
+        if self.device_available[ind-1]:
+            if name == "DelayTime":
+                return self.proxy[ind-1].read_attribute("DelayTime").value
+            elif name == "ExposureTime":
+                return self.proxy[ind-1].read_attribute("ExposureTime").value
+            elif name == "FileStartNum":
+                return self.proxy[ind-1].read_attribute("FileStartNum").value
+            elif name == "FilePrefix":
+                return self.proxy[ind-1].read_attribute("FilePrefix").value
+            elif name == "FileDir":
+                return self.proxy[ind-1].read_attribute("FileDir").value
+            elif name == "NbFrames":
+                return self.proxy[ind-1].read_attribute("NbFrames").value
+            elif name == "Reset":
+                if self.device_available[ind-1]:
+                    return 0
+            elif name == "TangoDevice":
+                tango_device = self.node + ":" + str(self.port) + "/" + self.proxy[ind-1].name() 
+                return tango_device
+
+    def SetExtraAttributePar(self,ind,name,value):
+#        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": In SetExtraFeaturePar method for index",ind," name=",name," value=",value
+        if self.device_available[ind-1]:
+            if name == "DelayTime":
+                self.proxy[ind-1].write_attribute("DelayTime",value)
+            elif name == "ExposureTime":
+                self.proxy[ind-1].write_attribute("ExposureTime",value)
+            elif name == "FileStartNum":
+                self.proxy[ind-1].write_attribute("FileStartNum",value)
+            elif name == "FilePrefix":
+                self.proxy[ind-1].write_attribute("FilePrefix",value)
+            elif name == "FileDir":
+                self.proxy[ind-1].write_attribute("FileDir",value)
+            elif name == "NbFrames":
+                self.proxy[ind-1].write_attribute("NbFrames",value)
+            elif name == "Reset":
+                if self.device_available[ind-1]:
+                    self.proxy[ind-1].command_inout("Reset")
+        
+    def SendToCtrl(self,in_data):
+#        print "Received value =",in_data
+        return "Nothing sent"
+        
+    def __del__(self):
+        print "PYTHON -> LCXCameraCtrl/",self.inst_name,": dying"
+
+        
+if __name__ == "__main__":
+    obj = TwoDController('test')
