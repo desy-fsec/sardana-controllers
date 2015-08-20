557712d486fc13487e758cc5fdb87e5a26945932
Teresa <tnunez@mail.desy.de>
2015-08-20 10:58:42 +0200

    Adding new controller

---
+++ python/countertimer/MHzDAQp01Ctrl.py
@@ -0,0 +1,133 @@
+import PyTango
+from sardana.pool.controller import CounterTimerController
+import time
+
+from sardana import State, DataAccess
+from sardana.pool.controller import MotorController
+from sardana.pool.controller import Type, Access, Description, DefaultValue
+from sardana.pool import PoolUtil
+
+ReadOnly = DataAccess.ReadOnly
+ReadWrite = DataAccess.ReadWrite
+
+global last_sta
+
+class MHzDAQp01Ctrl(CounterTimerController):
+    "This class is the Tango Sardana CounterTimer controller for the Mythen RoIs"
+	
+
+    axis_attributes = {'TangoDevice':{Type:str,Access:ReadOnly},
+                       }
+		     
+    ctrl_properties = {'RootDeviceName':{Type:str,Description:'The root name of the Mythenrois Tango devices'},
+                       'TangoHost':{Type:str,Description:'The tango host where searching the devices'}, 
+                       }
+
+    gender = "CounterTimer"
+    model = "MHzDAQp01"
+    organization = "DESY"
+    state = ""
+    status = ""
+    
+    def __init__(self,inst,props, *args, **kwargs):
+        self.TangoHost = None
+        CounterTimerController.__init__(self,inst,props, *args, **kwargs)
+        if self.TangoHost != None:
+            self.node = self.TangoHost
+            self.port = 10000
+            if self.TangoHost.find( ':'):
+                lst = self.TangoHost.split(':')
+                self.node = lst[0]
+                self.port = int( lst[1])
+        proxy_name = self.RootDeviceName
+        if self.TangoHost != None:
+            proxy_name = str(self.node) + (":%s/" % self.port) + str(proxy_name)
+        self.proxy = PyTango.DeviceProxy(proxy_name) 
+        global last_sta
+        last_sta = PyTango.DevState.ON
+
+    def AddDevice(self,ind):
+        CounterTimerController.AddDevice(self,ind)
+        
+    def DeleteDevice(self,ind):
+        CounterTimerController.DeleteDevice(self,ind)
+        self.proxy =  None
+        
+		
+    def StateOne(self,ind):
+        global last_sta
+        try:
+            sta = self.proxy.command_inout("State")
+            last_sta = sta
+        except:
+            sta = last_sta
+        if sta == PyTango.DevState.ON:
+            status_string = "MHzDAQp01 is in ON state"
+        elif sta == PyTango.DevState.MOVING:
+            status_string = "MHzDAQp01 is busy"
+        tup = (sta, status_string)
+        return tup
+
+    def PreReadAll(self):
+        pass
+        
+
+    def PreReadOne(self,ind):
+        pass
+
+
+    def ReadAll(self):
+        pass
+
+    def PreStartOne(self,ind,pos):
+        return True
+        
+    def StartOneCT(self,ind):
+        try:
+            sta = self.proxy.command_inout("State")
+        except:
+            sta = PyTango.DevState.ON
+        if sta == PyTango.DevState.ON:
+            self.proxy.command_inout("Start")
+            
+    def ReadOne(self,ind):
+        try:
+            if ind == 1:
+                value = self.proxy.read_attribute("MeanValue").value
+            else:
+                value = self.proxy.read_attribute("STDDevValue").value
+                
+        except:
+            value = -999
+        return  value
+	
+    def AbortOne(self,ind):
+        pass
+        
+    def PreStartAllCT(self):
+        self.wantedCT = []
+
+    def PreStartOneCT(self,ind):
+        pass
+	
+    def StartAllCT(self):
+        pass
+		     	
+    def LoadOne(self,ind,value):
+        pass
+	
+    def GetExtraAttributePar(self,ind,name):
+        if name == "TangoDevice":
+            tango_device = self.node + ":" + str(self.port) + "/" + self.proxy.name() 
+            return tango_device
+        
+            
+    def SetExtraAttributePar(self,ind,name,value):
+        pass
+			
+    def SendToCtrl(self,in_data):
+        return "Nothing sent"
+
+    def __del__(self):
+        print "PYTHON -> MHzDAQp01CtrlCtrl/",self.inst_name,": dying"
+
