70f7271609289c047eb3dfda7fd5641e33b597cf
teresa <tnunez@mail.desy.de>
2016-06-02 12:28:33 +0200

    Adding ctrl for interferometer

---
+++ python/countertimer/HasyInterferometerCtrl.py
@@ -0,0 +1,113 @@
+import PyTango
+from sardana.pool.controller import CounterTimerController
+import time
+
+from sardana import State, DataAccess
+from sardana.pool.controller import MotorController
+from sardana.pool.controller import Type, Access, Description, DefaultValue
+from sardana.pool import PoolUtil
+
+ReadOnly = DataAccess.ReadOnly
+ReadWrite = DataAccess.ReadWrite
+
+class HasyInterferometerCtrl(CounterTimerController):
+    "This class is the Tango Sardana CounterTimer controller for the Interferometers"
+	
+
+    axis_attributes = {'TangoDevice':{Type:str,Access:ReadOnly},
+                       }
+		     
+    ctrl_properties = {'RootDeviceName':{Type:str,Description:'The name of the Interferometer Tango device'},
+                       'TangoHost':{Type:str,Description:'The tango host where searching the device'}, 
+                       }
+
+    gender = "CounterTimer"
+    model = "Interferometer"
+    organization = "DESY"
+    state = ""
+    status = ""
+    
+    def __init__(self,inst,props, *args, **kwargs):
+        self.TangoHost = None
+        CounterTimerController.__init__(self,inst,props, *args, **kwargs)
+        if self.TangoHost != None:
+            self.node = self.TangoHost
+            self.port = 10000
+            if self.TangoHost.find( ':'):
+                lst = self.TangoHost.split(':')
+                self.node = lst[0]
+                self.port = int( lst[1])
+        self.started = False
+        self.time_to_set = 0
+        proxy_name = self.RootDeviceName
+        if self.TangoHost != None:
+            proxy_name = str(self.node) + (":%s/" % self.port) + str(proxy_name)
+        self.proxy = PyTango.DeviceProxy(proxy_name) 
+
+    def AddDevice(self,ind):
+        CounterTimerController.AddDevice(self,ind)
+        
+    def DeleteDevice(self,ind):
+        CounterTimerController.DeleteDevice(self,ind)
+        self.proxy =  None
+        
+		
+    def StateOne(self,ind):
+        sta = self.proxy.command_inout("State")
+        if sta == PyTango.DevState.RUNNING:
+            sta = PyTango.DevState.MOVING
+        tup = (sta, "Status from connected tango device")
+        return tup
+
+    def PreReadAll(self):
+        pass
+        
+
+    def PreReadOne(self,ind):
+        pass
+
+
+    def ReadAll(self):
+        pass
+
+    def PreStartOne(self,ind,pos):
+        return True
+        
+    def StartOneCT(self,ind):
+        pass
+            
+            
+    def ReadOne(self,ind):
+        value = -1
+        return  value
+	
+    def AbortOne(self,ind):
+        pass
+        
+    def PreStartAllCT(self):
+        self.wantedCT = []
+
+    def PreStartOneCT(self,ind):
+        pass
+	
+    def StartAllCT(self):
+        self.proxy.command_inout("CollectDataTime", self.time_to_set)
+		     	
+    def LoadOne(self,ind,value):
+        self.time_to_set = value * 1000 # in ms
+	
+    def GetExtraAttributePar(self,ind,name):
+        if name == "TangoDevice":
+            tango_device = self.node + ":" + str(self.port) + "/" + self.proxy.name() 
+            return tango_device
+        
+            
+    def SetExtraAttributePar(self,ind,name,value):
+        pass
+			
+    def SendToCtrl(self,in_data):
+        return "Nothing sent"
+
+    def __del__(self):
+        print "PYTHON -> HasyInterferometerCtrl/",self.inst_name,": dying"
+
