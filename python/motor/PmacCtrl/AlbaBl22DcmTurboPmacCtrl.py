ab061ede90c0ed088483f2196c93d411c8bd8fb7
Roberto J. Homs Puron <rhoms@cells.es>
2017-05-23 10:37:44 +0200

    Clean code
    Remove not used code

--- python/motor/PmacCtrl/AlbaBl22DcmTurboPmacCtrl.py
+++ python/motor/PmacCtrl/AlbaBl22DcmTurboPmacCtrl.py
@@ -31,12 +31,11 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program; if not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
-import math, logging, time
+import math
 import PyTango
-from sardana import pool
-from sardana.pool import PoolUtil
-from sardana.pool.controller import MotorController, Type, FSet, FGet, Access, Description, DataAccess, DefaultValue 
-
+import numpy as np
+from sardana.pool.controller import Type, FSet, FGet, Access, Description, \
+    DataAccess, DefaultValue
 from TurboPmacCtrl import TurboPmacController
 
 class DcmTurboPmacController(TurboPmacController):
@@ -102,50 +101,6 @@ class DcmTurboPmacController(TurboPmacController):
                    switchstate += 2
         return (state, status, switchstate)
 
-    #def StateOne(self, axis):
-        #self._log.debug("Entering StateOne")
-        #attributes = self.attributes[axis]
-        #switchstate = 0
-        #if not self.pmacEthOk:
-            #state = PyTango.DevState.ALARM
-            #status = "Ethernet connection with TurboPmac failed. \n(Check if PmacEth DS is running and if its state is ON)"
-        #elif not attributes["MotorActivated"]:
-            #state = PyTango.DevState.FAULT
-            #status = "Motor is deactivated - it is not under Pmac control (Check Ix00 variable)."
-        #else:
-            ##motion cases           
-            #if attributes["MotionProgramRunning"]:
-                #state = PyTango.DevState.MOVING
-                #status = "Motor is used by active motion program."                
-            #elif not attributes["DesiredVelocityZero"]:
-                #state = PyTango.DevState.MOVING
-                #status = "Motor is moving."
-            #else:
-                #state = PyTango.DevState.ON
-                #status = "Motor is holding it's position."
-            #if state == PyTango.DevState.MOVING and attributes["HomeSearchInProgress"]:
-                #status += "Home search in progress."
-
-            ##amplifier fault cases
-            #if not attributes["AmplifierEnabled"]:
-                #state = PyTango.DevState.ALARM
-                #status = "Amplifier disabled."
-                #if attributes["AmplifierFaultError"]:
-                    #status += "\nAmplifier fault signal received."
-                #if attributes["FatalFollowingError"]:
-                    #status += "\nFatal Following / Integrated Following Error exceeded."
-            ##limits cases        
-            #if attributes["NegativeEndLimitSet"]:
-                   #state = PyTango.DevState.ALARM
-                   #status += "\nAt least one of the lower/upper switches is activated"
-                   #switchstate += 4
-            #if attributes["PositiveEndLimitSet"]:
-                   #state = PyTango.DevState.ALARM
-                   #status += "\nAt least one of the negative/positive limit is activated"
-                   #switchstate += 2
-        #self._log.debug("Leaving StateOne")
-        #return (state, status, switchstate)
-       
     def StartAll(self):
         self._log.debug("Entering StartAll")
         #@todo: here we should use some extra_attribute of energy pseudomotor saying if we want to do energy motion or single axis motion, cause len(self.startMultiple) > 1 is true if we move a MotorGroup(e.g. mv macro with bragg and perp) 
