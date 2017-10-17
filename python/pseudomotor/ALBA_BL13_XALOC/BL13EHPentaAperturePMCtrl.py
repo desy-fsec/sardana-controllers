282c946c552ad9fbd9b5463f908d6919dd6b6aaa
Jordi Andreu <jandreu@cells.es>
2017-10-17 17:29:20 +0200

    Refactor Penta Aperture controller
    Major changes on the Penta Aperture controller to
    fix calibration matrix management.

--- python/pseudomotor/ALBA_BL13_XALOC/BL13EHPentaAperturePMCtrl.py
+++ python/pseudomotor/ALBA_BL13_XALOC/BL13EHPentaAperturePMCtrl.py
@@ -8,6 +8,7 @@ CALIBRATION = 'Calibration'
 LABELS = 'Labels'
 NLABELS = 'Nlabels'
 
+
 class BL13EHPentaApertureController(DiscretePseudoMotorController):
     """
     Discrete controller which provides a pseudomotor as a function of
@@ -21,137 +22,149 @@ class BL13EHPentaApertureController(DiscretePseudoMotorController):
     pseudo_motor_roles = ('pseudo',)
     motor_roles = ('role1', 'role2',)
 
-    axis_attributes = {CALIBRATION:
-                           {Type: str,
-                            Description: '3-level nested list. List of valid' \
-                                         'positions for each pseudomotor role and each' \
-                                         'position element is a list which provides' \
-                                         '[min, cal, max] for each motor role',
-                            Access: DataAccess.ReadWrite,
-                            'fget': 'get%s' % CALIBRATION,
-                            'fset': 'set%s' % CALIBRATION},
-                       LABELS:
-                           {Type: str,
-                            Description: 'String list of elements with shape' \
-                                         '<str:value>. The <str> represents the pseudo-' \
-                                         'motor position with value <value>',
-                            Access: DataAccess.ReadWrite,
-                            'fget': 'get%s' % LABELS,
-                            'fset': 'set%s' % LABELS},
-                       NLABELS:
-                           {Type: int,
-                            Description: 'Number of labels available',
-                            Access: DataAccess.ReadOnly,
-                            'fget': 'get%s' % NLABELS}
-                       }
+    axis_attributes = {
+        CALIBRATION:
+            {Type: str,
+             Description: '3-level nested list. List of valid' \
+                          'positions for each pseudomotor role and each' \
+                          'position element is a list which provides' \
+                          '[min, cal, max] for each motor role',
+             Access: DataAccess.ReadWrite,
+             'fget': 'get%s' % CALIBRATION,
+             'fset': 'set%s' % CALIBRATION},
+        LABELS:
+            {Type: str,
+             Description: 'String list of elements with shape' \
+                          '<str:value>. The <str> represents the pseudo-' \
+                          'motor position with value <value>',
+             Access: DataAccess.ReadWrite,
+             'fget': 'get%s' % LABELS,
+             'fset': 'set%s' % LABELS},
+        NLABELS:
+            {Type: int,
+             Description: 'Number of labels available',
+             Access: DataAccess.ReadOnly,
+             'fget': 'get%s' % NLABELS}
+    }
 
     def CalcPseudo(self, axis, physical_pos, curr_pseudo_pos):
-        llabels = len(self._labels)
         positions = self._positions
         calibration = self._calibration
-        lcalibration = self._CheckCalibrationMatrix(calibration)
+        cm = CalibrationMatrix(calibration)
 
-        # case 0: nothing to translate, only round about integer each attribute value
-        if llabels == 0:
-            raise Exception('Invalid label configuration')
-        # case 1: only uses the labels. Available positions in POSITIONS
-        elif lcalibration == 0:
-            index = []
-            for pos in physical_pos:
-                try:
-                    index.append(positions.index(int(pos)))
-                except:
-                    raise Exception('Index is out of range.')
-            if self._CheckIndexPos(index, physical_pos):
-                msg = 'New pseudo position from index: %s' % index[0]
-                self._log.info()
-                return index[0]
-            else:
-                raise Exception('Invalid index for pseudo position.')
-                # case 1+fussy: the physical position must be in one of the defined
-        # ranges, and the DiscretePseudoMotor position is defined in labels
-        elif llabels == lcalibration:
-            # Each calibration MUST return the same position i.e. the same index
-            # Collect the calibration index for each motor role
-            index = []
-            for pos, cal in zip(physical_pos, calibration):
-                for fussyPos in cal:
-                    if pos >= fussyPos[0] and pos <= fussyPos[2]:
-                        index.append(cal.index(fussyPos))
-            if self._CheckIndexPos(index, physical_pos):
-                msg = 'New position from calibration: %s' % (positions[index[0]])
-                self._log.info(msg)
-                return positions[index[0]]
-            else:
-                raise Exception('Invalid values returned from calibration.')
+        llabels = len(self._labels)
+        lcalibration = cm.axis_dim
+
+        if llabels == lcalibration:
+            idx = cm.get_pseudo_pos_index(physical_pos)
+            self._log.debug("Index found is %s" % idx)
+            return positions[int(idx)]
         else:
-            raise Exception('CalcPseudo failed.')
+            msg = "labels = %s, points per axis = %s" % (llabels, lcalibration)
+            self._log.error(msg)
+            raise Exception('CalcPseudo failed.\n%s' % msg)
 
     def CalcAllPhysical(self, pseudo_pos, curr_physical_pos):
-        # If Labels is well defined, the write value must be one this struct
-        llabels = len(self._labels)
         positions = self._positions
         calibration = self._calibration
-        lcalibration = self._CheckCalibrationMatrix(calibration)
+        cm = CalibrationMatrix(calibration)
+
+        llabels = len(self._labels)
+        lcalibration = cm.axis_dim
+
         value = pseudo_pos[0]
         pos_list = []
 
-        # case 0: nothing to translate:
-        # Return a list with the same value to each physical
-        if llabels == 0:
-            for m in range(len(self.motor_roles)):
-                pos_list.append(value)
-            return pos_list
-        # case 1: only uses the labels. Available positions in POSITIONS
-        elif lcalibration == 0:
-            for pos in positions:
-                try:
-                    pos.index(value)
-                except:
-                    raise Exception('Invalid position index.')
-                pos_list.append(value)
-            return pos_list
-        # case 1+fussy: the write to the to the DiscretePseudoMotorController
-        # is translated to the central position of the calibration.
-        elif llabels == lcalibration:
-            index = None
+        if llabels == lcalibration:
             # Get index position corresponding to pseudo position
             try:
                 index = positions.index(value)
-            except:
+            except Exception:
                 raise Exception('Non valid pseudo position.')
 
             # Create list of physical positions from calibration
-            for cal in calibration:
-                pos_list.append(cal[index][1])
+            for cal in cm.axis_cal:
+                pos_list.append(cal[index].value)
             msg = 'Index %s generated position list = %s' %\
                   (index, repr(pos_list))
             self._log.debug(msg)
             return pos_list
 
-    def _CheckCalibrationMatrix(self, calibration):
-        # Check the integrity of the calibration matrix
-        # A calibration list of triplets MUST exist for each motor role
-        # Each calibration list MUST have the same number of elements and
-        # the same length as the pseudomotor labels.
-        # Returns the length of each calibration (which MUST have the same value)
-        lcal_list = []
-        if len(calibration) == len(self.motor_roles):
-            for cal in calibration:
-                lcal_list.append(len(cal))
-            if all(lcal_list[0] == c for c in lcal_list):
-                return lcal_list[0]
-        else:
-            raise Exception('Invalid calibration matrix.')
 
-    def _CheckIndexPos(self, index, target_pos):
-        return (
-            len(index) == len(target_pos) and all(index[0] == i for i in index))
+class CalibrationMatrix(object):
+    def __init__(self, calibrations):
+        # Extract calibration by axis as arrays of fuzzy points.
+        self.axis_cal = []
+        for c in calibrations:
+            cal = []
+            for point in c:
+                # A calibration list of triplets MUST exist for each motor role
+                if len(point) == 3:
+                    cal.append(FuzzyPoint(*point))
+                else:
+                    msg = "Position point has no valid structure" \
+                          " min/pos/max\n dimension is %s" % len(p)
+                    raise Exception(msg)
+            # append new axis calibration
+            self.axis_cal.append(cal)
+
+        # Store ALL calibration dimensions
+        lcal = []
+        for c in self.axis_cal:
+            lcal.append(len(c))
+
+        # Each calibration MUST have the same number of positions
+        if not all(lcal[0] == c for c in lcal):
+            msg = "Inconsistent axes dimensions.\n%s" % repr(lcal)
+            raise Exception(msg)
+
+        self.n_axis = len(self.axis_cal)
+        self.axis_dim = lcal[0]
+
+    def get_pseudo_pos_index(self, positions):
+        # Each calibration MUST return the same position i.e. the same index
+        # Collect the calibration index for each motor role
+        for i in range(self.axis_dim):
+            found = []
+            for pos, cal in zip(positions, self.axis_cal):
+                res = cal[i].includes(pos)
+                found.append(res)
+            if all(found):
+                return i
+        return float('NaN')
+
+    def __repr__(self):
+        _str = ""
+        for e, c in enumerate(self.axis_cal):
+            _str += "\naxis %-2s --> " % e
+            for p in c:
+                _str += "%-20s " % p
+        return _str
 
 
-    def getNlabels(self,axis):
-        #hackish until we support DevVarDoubleArray in extra attrs
-        pos = self._positions
-        n = len(pos)
-        self._log.debug('Available pseudo positions (%s) = %s' % (n, repr(pos)))
-        return n
+class FuzzyPoint(object):
+    def __init__(self, min, value, max):
+        self.min = min
+        self.value = value
+        self.max = max
+
+    def includes(self, value):
+        return self.min <= value <= self.max
+
+    def __repr__(self):
+        return "[%s, %s, %s]" % (self.min, self.value, self.max)
+
+
+def main(pt):
+
+    cm = CalibrationMatrix([[[0.9, 1.0, 1.1], [1.9, 2.0, 2.1], [2.9, 3.0, 3.1]],
+                            [[9.0, 10.0, 11.0], [19.0, 20.0, 21.0],
+                             [29.0, 30.0, 31.0]]])
+    print "\nCalibration matrix:"
+    print cm
+    print "\nPseudo = %s\n" % cm.get_pseudo_pos_index(pt)
+
+
+if __name__ == "__main__":
+    p = [3.0, 30.0]
+    main(p)
