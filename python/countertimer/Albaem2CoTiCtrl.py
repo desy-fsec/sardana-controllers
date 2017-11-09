1414b0d25a200516bfc88c934352c72bb5d8d58d
Roberto Javier Homs Puron <rhoms@cells.es>
2017-11-09 16:03:50 +0100

    Add Range and Inversion attribute
    Add Range and Inversion extra axis attributes.
    Fix flake8 errors.

--- python/countertimer/Albaem2CoTiCtrl.py
+++ python/countertimer/Albaem2CoTiCtrl.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python
 import time
+import socket
 
-import PyTango
 from sardana import State, DataAccess
 from sardana.sardanavalue import SardanaValue
 from sardana.pool import AcqSynch
-from sardana.tango.core.util import from_tango_state_to_state
-from sardana.pool.controller import (CounterTimerController, Type, Access,
-                                     Description, NotMemorized, Memorize, 
-                                     Memorized)
-import socket 
-import time
+from sardana.pool.controller import CounterTimerController, Type, Access, \
+    Description, Memorize, Memorized, NotMemorized
 
 __all__ = ['Albaem2CoTiCtrl']
 
-TRIGGER_INPUTS ={'DIO_1': 0,'DIO_2': 1,'DIO_3': 2, 'DIO_4': 3, 'DIFF_IO_1': 4,
-                 'DIFF_IO_2': 5, 'DIFF_IO_3': 6,'DIFF_IO_4': 7 ,'DIFF_IO_5': 8,
-                 'DIFF_IO_6': 9, 'DIFF_IO_7': 10,'DIFF_IO_8': 11,'DIFF_IO_9': 12}
+TRIGGER_INPUTS = {'DIO_1': 0, 'DIO_2': 1, 'DIO_3': 2, 'DIO_4': 3,
+                  'DIFF_IO_1': 4, 'DIFF_IO_2': 5, 'DIFF_IO_3': 6,
+                  'DIFF_IO_4': 7, 'DIFF_IO_5': 8, 'DIFF_IO_6': 9,
+                  'DIFF_IO_7': 10, 'DIFF_IO_8': 11, 'DIFF_IO_9': 12}
 
 
 class Albaem2CoTiCtrl(CounterTimerController):
-
 
     MaxDevice = 5
 
     ctrl_properties = {'AlbaEmHost': {'Description': 'AlbaEm Host name',
                                       'Type': 'PyTango.DevString'},
                        'Port': {'Description': 'AlbaEm Host name',
-                                      'Type': 'PyTango.Integer'},
+                                'Type': 'PyTango.Integer'},
                        }
 
     ctrl_attributes = {
@@ -38,27 +34,20 @@ class Albaem2CoTiCtrl(CounterTimerController):
             Memorize: Memorized},
         }
 
+    axis_attributes = {"Range": {
+                            Type: str,
+                            Description: 'Range for the channel',
+                            Memorize: NotMemorized,
+                            Access: DataAccess.ReadWrite,
+                            },
+                       "Inversion": {
+                            Type: bool,
+                            Description: 'Channel Digital inversion',
+                            Memorize: NotMemorized,
+                            Access: DataAccess.ReadWrite,
 
-#     axis_attributes = {"Range": {
-#                             Type: str,
-#                             Description: 'Range for the channel',
-#                             Memorize: NotMemorized,
-#                             Access: DataAccess.ReadWrite,
-#                             },
-#                        "Filter": {
-#                             Type: str,
-#                             Description: 'Filter for the channel',
-#                             Memorize: NotMemorized,
-#                             Access: DataAccess.ReadWrite,
-#                             },
-#                        "Inversion": {
-#                             Type: str,
-#                             Description: 'Channel Digital inversion',
-#                             Memorize: NotMemorized,
-#                             Access: DataAccess.ReadWrite,
-
-#                             },
-#                        }
+                            },
+                       }
 
     def __init__(self, inst, props, *args, **kwargs):
         """Class initialization."""
@@ -72,7 +61,7 @@ class Albaem2CoTiCtrl(CounterTimerController):
         self.albaem_socket.connect(ip_config)
         self.index = 0
         self.master = None
-        self._latency_time = 0.001 # In fact, it is just 320us
+        self._latency_time = 0.001  # In fact, it is just 320us
 
     def AddDevice(self, axis):
         """Add device to controller."""
@@ -84,57 +73,65 @@ class Albaem2CoTiCtrl(CounterTimerController):
     def DeleteDevice(self, axis):
         """Delete device from the controller."""
         self._log.debug("DeleteDevice(%d): Entering...", axis)
-        #self.albaem_socket.close()
+        # self.albaem_socket.close()
 
     def StateAll(self):
         """Read state of all axis."""
-        #self._log.debug("StateAll(): Entering...")
+        # self._log.debug("StateAll(): Entering...")
         state = self.sendCmd('ACQU:STAT?')
-        if state in ['STATE_ACQUIRING', 'STATE_RUNNING']: 
+
+        if state in ['STATE_ACQUIRING', 'STATE_RUNNING']:
             self.state = State.Moving
-        elif state == 'STATE_ON': 
+
+        elif state == 'STATE_ON':
             self.state = State.Standby
+
         elif state == 'STATE_FAULT':
             self.state = State.Fault
+
         else:
             self.state = State.Fault
-            self._log.debug("StateAll(): %r %r UNKNWON STATE: %s" %(self.state, self.status), state)
+            self._log.debug("StateAll(): %r %r UNKNWON STATE: %s" % (
+                self.state, self.status), state)
         self.status = state
-        #self._log.debug("StateAll(): %r %r" %(self.state, self.status))
+        # self._log.debug("StateAll(): %r %r" %(self.state, self.status))
 
     def StateOne(self, axis):
         """Read state of one axis."""
-        #self._log.debug("StateOne(%d): Entering...", axis)
+        # self._log.debug("StateOne(%d): Entering...", axis)
         return self.state, self.status
 
     def LoadOne(self, axis, value, repetitions):
-        #self._log.debug("LoadOne(%d, %f, %d): Entering...", axis, value, repetitions)
+        # self._log.debug("LoadOne(%d, %f, %d): Entering...", axis, value,
+        #                 repetitions)
         if axis != 1:
-            raise Execption('The master channel should be the axis 1')
+            raise Exception('The master channel should be the axis 1')
 
         self.itime = value
         self.index = 0
-        # Set Integration time in ms  
-        val = int(self.itime * 1000)        
+
+        # Set Integration time in ms
+        val = int(self.itime * 1000)
         self.sendCmd('ACQU:TIME %r' % val, rw=False)
 
-        if self._synchronization in [AcqSynch.SoftwareTrigger, AcqSynch.SoftwareGate]:
-            #self._log.debug("SetCtrlPar(): setting synchronization "
-            #"to SoftwareTrigger")
+        if self._synchronization in [AcqSynch.SoftwareTrigger,
+                                     AcqSynch.SoftwareGate]:
+            # self._log.debug("SetCtrlPar(): setting synchronization "
+            #                 "to SoftwareTrigger")
             repetitions = 1
             source = 'SOFTWARE'
+
         else:
-            #self._log.debug("SetCtrlPar(): setting synchronization "
-            #"to HardwareTrigger")
+            # self._log.debug("SetCtrlPar(): setting synchronization "
+            #                 "to HardwareTrigger")
             source = 'HARDWARE'
         self.sendCmd('TRIG:MODE %s' % source, rw=False)
 
         # Set Number of Triggers
-        self.sendCmd('ACQU:NTRI %r' % repetitions, rw=False) 
-
+        self.sendCmd('ACQU:NTRI %r' % repetitions, rw=False)
 
     def PreStartOneCT(self, axis):
-        #self._log.debug("PreStartOneCT(%d): Entering...", axis)
+        # self._log.debug("PreStartOneCT(%d): Entering...", axis)
         if axis != 1:
             self.index = 0
         return True
@@ -144,17 +141,20 @@ class Albaem2CoTiCtrl(CounterTimerController):
         Starting the acquisition is done only if before was called
         PreStartOneCT for master channel.
         """
-        #self._log.debug("StartAllCT(): Entering...")
+        # self._log.debug("StartAllCT(): Entering...")
         cmd = 'ACQU:START'
-        if self._synchronization in [AcqSynch.SoftwareTrigger, AcqSynch.SoftwareGate]:
+        if self._synchronization in [AcqSynch.SoftwareTrigger,
+                                     AcqSynch.SoftwareGate]:
             # The HW needs the software trigger
-            # APPEND SWTRIG TO THE START COMMAND OR SEND ANOTHER COMMAND TRIG:SWSEt
+            # APPEND SWTRIG TO THE START COMMAND OR SEND ANOTHER COMMAND
+            # TRIG:SWSEt
             cmd += ' SWTRIG'
 
         self.sendCmd(cmd, rw=False)
-        ### THIS PROTECTION HAS TO BE REVIEWED
-        ### FAST INTEGRATION TIMES MAY RAISE WRONG EXCEPTIONS
-        ### e.g. 10ms ACQTIME -> self.state MAY BE NOT MOVING BECAUSE FINISHED, NOT FAILED
+        # THIS PROTECTION HAS TO BE REVIEWED
+        # FAST INTEGRATION TIMES MAY RAISE WRONG EXCEPTIONS
+        # e.g. 10ms ACQTIME -> self.state MAY BE NOT MOVING BECAUSE
+        # FINISHED, NOT FAILED
         self.StateAll()
         t0 = time.time()
         while (self.state != State.Moving):
@@ -164,8 +164,8 @@ class Albaem2CoTiCtrl(CounterTimerController):
         return True
 
     def ReadAll(self):
-        #self._log.debug("ReadAll(): Entering...")        
-        #TODO Change the ACQU:MEAS command by CHAN:CURR
+        # self._log.debug("ReadAll(): Entering...")
+        # TODO Change the ACQU:MEAS command by CHAN:CURR
         data_ready = int(self.sendCmd('ACQU:NDAT?'))
         self.new_data = []
         try:
@@ -173,30 +173,32 @@ class Albaem2CoTiCtrl(CounterTimerController):
                 data_len = data_ready - self.index
                 # THIS CONTROLLER IS NOT YET READY FOR TIMESTAMP DATA
                 self.sendCmd('TMST 0', rw=False)
-                raw_data = self.sendCmd('ACQU:MEAS? %r,%r' % (self.index-1,data_len))
+                raw_data = self.sendCmd('ACQU:MEAS? %r,%r' % (self.index-1,
+                                                              data_len))
                 data = eval(raw_data)
                 for chn_name, values in data:
                     self.new_data.append(eval(values))
                 time_data = [self.itime] * len(self.new_data[0])
-                self.new_data.insert(0,time_data)
+                self.new_data.insert(0, time_data)
                 self.index += len(time_data)
 
         except Exception as e:
             raise Exception("ReadAll error: %s: "+str(e))
 
     def ReadOne(self, axis):
-        #self._log.debug("ReadOne(%d): Entering...", axis)
+        # self._log.debug("ReadOne(%d): Entering...", axis)
         if len(self.new_data) == 0:
             return []
 
-        if self._synchronization in [AcqSynch.SoftwareTrigger, AcqSynch.SoftwareGate]:
+        if self._synchronization in [AcqSynch.SoftwareTrigger,
+                                     AcqSynch.SoftwareGate]:
             return SardanaValue(self.new_data[axis-1][0])
         else:
             val = self.new_data[axis-1]
             return val
 
     def AbortOne(self, axis):
-        #self._log.debug("AbortOne(%d): Entering...", axis)
+        # self._log.debug("AbortOne(%d): Entering...", axis)
         self.sendCmd('ACQU:STOP', rw=False)
 
     def sendCmd(self, cmd, rw=True, size=8096):
@@ -245,10 +247,41 @@ class Albaem2CoTiCtrl(CounterTimerController):
                     break
             return data[:-2]
 
+###############################################################################
+#                Axis Extra Attribute Methods
+###############################################################################
+    def GetExtraAttributePar(self, axis, name):
+        self._log.debug("GetExtraAttributePar(%d, %s): Entering...", axis,
+                        name)
+        if axis == 1:
+            raise ValueError('The axis 1 does not use the extra attributes')
 
-################################################################################
+        name = name.lower()
+        axis -= 1
+        if name == "range":
+            cmd = 'CHAN{0:02d}:CABO:RANGE?'.format(axis)
+            return self.sendCmd(cmd)
+        elif name == 'inversion':
+            cmd = 'CHAN{0:02d}:CABO:INVE?'.format(axis)
+            return eval(self.sendCmd(cmd))
+
+    def SetExtraAttributePar(self, axis, name, value):
+        if axis == 1:
+            raise ValueError('The axis 1 does not use the extra attributes')
+
+        name = name.lower()
+        axis -= 1
+        if name == "range":
+            cmd = 'CHAN{0:02d}:CABO:RANGE {1}'.format(axis, value)
+            self.sendCmd(cmd, rw=False)
+        elif name == 'inversion':
+            cmd = 'CHAN{0:02d}:CABO:INVE {1}'.format(axis, int(value))
+            self.sendCmd(cmd, rw=False)
+
+
+###############################################################################
 #                Controller Extra Attribute Methods
-################################################################################
+###############################################################################
     def SetCtrlPar(self, parameter, value):
         param = parameter.lower()
         if param == 'exttriggerinput':
@@ -268,25 +301,23 @@ class Albaem2CoTiCtrl(CounterTimerController):
 if __name__ == '__main__':
     host = 'electproto22'
     port = 5025
-    ctrl = Albaem2CoTiCtrl('test', {'AlbaEmHost': host, 'Port': port })
+    ctrl = Albaem2CoTiCtrl('test', {'AlbaEmHost': host, 'Port': port})
     ctrl.AddDevice(1)
     ctrl.AddDevice(2)
     ctrl.AddDevice(3)
     ctrl.AddDevice(4)
     ctrl.AddDevice(5)
 
-
     ctrl._synchronization = AcqSynch.SoftwareTrigger
-    #ctrl._synchronization = AcqSynch.HardwareTrigger
+    # ctrl._synchronization = AcqSynch.HardwareTrigger
     acqtime = .03
     ctrl.LoadOne(1, acqtime, 10)
     ctrl.StartAllCT()
     t0 = time.time()
     ctrl.StateAll()
-    while ctrl.StateOne(1)[0] !=  State.Standby:
+    while ctrl.StateOne(1)[0] != State.Standby:
         ctrl.StateAll()
         time.sleep(0.1)
     print time.time() - t0 - acqtime
     ctrl.ReadAll()
     print ctrl.ReadOne(2)
-    
