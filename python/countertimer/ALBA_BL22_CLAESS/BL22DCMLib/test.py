0b10473d384d39730a86010ada79136f7290521d
Roberto Javier Homs Puron <rhoms@cells.es>
2018-01-09 17:37:50 +0100

    Add test to BL22 DCM library

--- python/countertimer/ALBA_BL22_CLAESS/BL22DCMLib/test.py
+++ python/countertimer/ALBA_BL22_CLAESS/BL22DCMLib/test.py
@@ -43,3 +43,23 @@ if __name__ == '__main__':
     print bl22dcmlib.getbragg(rawCounts[0], bragg_spu,
                                                bragg_offset, bragg_pos,
                                                bragg_enc)
+
+    rawCounts = np.array([8381945])
+    vcm_pitch_rad = 0.0038029889762572888
+    xtal_d = 3.1354161
+    xtal_offset = -0.0108689042918572
+    bragg_spu = 200000.0
+    bragg_offset = 0.075
+    bragg_pos = 8382465.0
+    bragg_enc = 8381947.0
+
+    print 'Should be:', 2944.88038418
+    print bl22dcmlib.enegies4encoders(rawCounts, vcm_pitch_rad,
+                                      xtal_d,
+                                      xtal_offset, bragg_spu,
+                                      bragg_offset, bragg_pos,
+                                      bragg_enc)
+
+    print 'Should be:', 41.987325
+    print bl22dcmlib.getbragg(rawCounts[0], bragg_spu,
+                              bragg_offset, bragg_pos, bragg_enc)
