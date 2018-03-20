d954708ba58b89d5df3c0d1c7da5641c648b519a
teresa <tnunez@mail.desy.de>
2018-03-20 14:58:11 +0100

    Adding support for SIS3302Master class

--- python/oned/SIS3302Ctrl.py
+++ python/oned/SIS3302Ctrl.py
@@ -19,7 +19,8 @@ class SIS3302Ctrl(OneDController):
                          }
     
     class_prop = {'RootDeviceName':{Type:'PyTango.DevString',Description:'The root name of the MCA Tango devices'},
-                  'TangoHost':{Type:str,Description:'The tango host where searching the devices'}, }
+                  'TangoHost':{Type:str,Description:'The tango host where searching the devices'},
+                  "type": {Type: str, Description: '1 if controlling SIS3302Master', DefaultValue: 0}, }
     
     MaxDevice = 97
 
@@ -80,14 +81,18 @@ class SIS3302Ctrl(OneDController):
     def ReadAll(self):
         #while(self.proxy.command_inout("State") != PyTango.DevState.ON):
         #    sleep(0.1)
-        self.counts = self.proxy.read_attribute("Count").value
+        if self.Type == 0:
+            self.counts = self.proxy.read_attribute("Count").value
 
     def ReadOne(self,ind):
         #print "SIS3302Ctrl.ReadOne",self.inst_name,"index",ind
-        if ind == 1:            
-            data = self.proxy.Data
+        if self.Type == 0:
+            if ind == 1:            
+                data = self.proxy.Data
+            else:
+                data = [self.counts[ind - 2]]
         else:
-            data = [self.counts[ind - 2]]
+            data = [self.proxy.read_attribute("CountsOfAllChannels").value]
         return data
 
     def PreStartAll(self):
