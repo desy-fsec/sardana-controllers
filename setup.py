a5dca38e7f1370e7337e52101a68b420a91dad6f
Jan Kotanski <jankotan@gmail.com>
2020-06-02 17:01:48 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+import imp
+from setuptools import setup
+from datetime import datetime
+
+release = datetime.today().strftime('%Y.%m.%d.%H.%M').replace(".0", ".")
+
+package_dir = {"sardana.PoolController": "python"}
+
+packages = ["sardana.PoolController"]
+
+provides = ['python']
+
+
+setup(name='PoolController',
+      version=release,
+      author="Sardana Controller Developers",
+      author_email="fs-ec@desy.de",
+      maintainer="DESY",
+      maintainer_email="fs-ec@desy.de",
+      url="https://sourceforge.net/u/tere29/sardanacontrollers/ci/master/tree/",
+      packages=packages,
+      package_dir=package_dir,
+      include_package_data=True,
+      provides=provides,
+      )
